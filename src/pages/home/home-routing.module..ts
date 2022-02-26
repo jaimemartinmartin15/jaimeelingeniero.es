408a3f723ccbdee0efd2f3eac7e6f0a3af10ade9
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-02-26 20:05:18 +0100

    Merge branch 'develop'

--- src/pages/home/home-routing.module..ts
+++ src/pages/home/home-routing.module..ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('../pages/home/home.module').then(m => m.HomeModule),
+    component: HomeComponent
   },
   {
     path: '**',
@@ -14,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule { }
