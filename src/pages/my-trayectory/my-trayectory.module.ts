5cd67adb541bea1016bc2c88d53d8e752b366ca2
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-03-04 16:53:43 +0100

    Created my-trayectory module and component

---
+++ src/pages/my-trayectory/my-trayectory.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyTrayectoryComponent } from './my-trayectory.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MyTrayectoryComponent]
+})
+export class MyTrayectoryModule { }
