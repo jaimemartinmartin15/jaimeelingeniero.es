f534251336819fd7a4020083dc66182025a16584
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-03-04 17:01:55 +0100

    Added routing to my-trayectory component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'mi-trayectoria',
+    loadChildren: () => import('../pages/my-trayectory/my-trayectory.module').then(m => m.MyTrayectoryModule),
+  },
+  {
     path: '',
     loadChildren: () => import('../pages/home/home.module').then(m => m.HomeModule),
   },
--- src/pages/my-trayectory/my-trayectory.module.ts
+++ src/pages/my-trayectory/my-trayectory.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MyTrayectoryComponent } from './my-trayectory.component';
+import { MyTrayectoryRoutingModule } from './my-trayectory-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MyTrayectoryRoutingModule
   ],
   declarations: [MyTrayectoryComponent]
 })
