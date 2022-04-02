3269791e8638b44fc17a847a90f9afa6cf1b63c4
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-02 16:24:07 +0200

    Setup rxjs observable page

---
+++ src/pages-rxjs/observable/observable.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-observable',
+  templateUrl: './observable.component.html',
+  styleUrls: ['./observable.component.scss']
+})
+export class ObservableComponent {}
---
+++ src/pages-rxjs/observable/observable.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ObservableComponent } from './observable.component';
+import { ObservableRoutingModule } from './observable-routing.module';
+
+@NgModule({
+  imports: [CommonModule, ObservableRoutingModule],
+  declarations: [ObservableComponent]
+})
+export class ObservableModule {}
