a4cd3f4afcfc50264bb190d00fa186d986fc0a55
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-04 18:41:53 +0200

    Created conveyor-controller component

--- src/pages-rxjs/shared/components/rxjs-components.module.ts
+++ src/pages-rxjs/shared/components/rxjs-components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConveyorComponent } from './conveyor/conveyor.component';
+import { ConveyorControllerComponent } from './conveyor-controller/conveyor-controller.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ConveyorComponent],
-  exports: [ConveyorComponent],
+  declarations: [ConveyorComponent, ConveyorControllerComponent],
+  exports: [ConveyorComponent, ConveyorControllerComponent],
 })
 export class RxjsComponentsModule {}
