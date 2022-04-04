e9dc89922bc9f2d7191b07d3f9a76b76cef8dc3c
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-04 21:04:42 +0200

    Edited subscriber component

--- src/pages-rxjs/shared/components/subscriber/subscriber.component.ts
+++ src/pages-rxjs/shared/components/subscriber/subscriber.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-subscriber',
   templateUrl: './subscriber.component.svg',
   styleUrls: ['./subscriber.component.scss'],
 })
-export class SubscriberComponent {}
+export class SubscriberComponent {
+  @Input()
+  public onSubscribe$ = new EventEmitter<void>();
+}
