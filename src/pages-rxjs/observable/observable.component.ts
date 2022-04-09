94b533c5b6336e87227efb845fd18503aa53dde4
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-09 16:52:17 +0200

    Coordinate observable demo and events

--- src/pages-rxjs/observable/observable.component.ts
+++ src/pages-rxjs/observable/observable.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { ElemementInConveyor, SpeechBubble } from '../shared/constants';
 
 @Component({
   selector: 'app-observable',
@@ -8,21 +9,47 @@ import { BehaviorSubject, Subject } from 'rxjs';
 })
 export class ObservableComponent {
   public conveyorWorking$ = new BehaviorSubject<boolean>(false);
-  public addToConveyor$ = new Subject<any>();
+  public addToConveyor$ = new Subject<Pick<ElemementInConveyor, 'type' | 'value'>>();
+  public speechBubble$ = new Subject<SpeechBubble>();
 
-  public subscribe() {
-    this.conveyorWorking$.next(true);
+  public onSubscribe(isSubscribed: boolean) {
+    this.conveyorWorking$.next(isSubscribed);
   }
 
   public onClickNext() {
-    this.addToConveyor$.next('🍎');
+    this.addToConveyor$.next({
+      type: 'next',
+      value: '🍎',
+    });
   }
 
   public onClickError() {
-    this.addToConveyor$.next('🍌');
+    this.addToConveyor$.next({
+      type: 'error',
+      value: '🍌',
+    });
   }
 
   public onClickComplete() {
-    this.conveyorWorking$.next(false);
+    this.addToConveyor$.next({
+      type: 'complete',
+      value: '🖐️',
+    });
+  }
+
+  public onEventDelivered(element: ElemementInConveyor) {
+    if (element.type === 'error' || element.type === 'complete') {
+      this.speechBubble$.next({
+        message: element.value,
+        color: element.type === 'error' ? '#d00' : '#ffdd00',
+      });
+      this.conveyorWorking$.next(false);
+    } else {
+      this.conveyorWorking$.next(true);
+      this.speechBubble$.next({
+        message: element.value,
+        color: '#0a0',
+      });
+    }
   }
 }
