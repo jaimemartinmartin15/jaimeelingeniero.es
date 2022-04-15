f3ab3a45009a60305b765312ad2fdf06128b773d
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-15 16:13:53 +0200

    Refactor rxjs components

--- src/pages-rxjs/shared/components/conveyor/conveyor.component.ts
+++ src/pages-rxjs/shared/components/conveyor/conveyor.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BehaviorSubject, distinctUntilChanged, filter, interval, Observable, Subscription } from 'rxjs';
-import { ElemementInConveyor } from '../../constants';
+import { ElemementInConveyor } from '../../element-in-conveyor';
+import { ObservableEventType } from '../../observable-event-type';
 
 @Component({
   selector: 'app-conveyor',
@@ -8,11 +9,21 @@ import { ElemementInConveyor } from '../../constants';
   styleUrls: ['./conveyor.component.scss'],
 })
 export class ConveyorComponent implements OnInit {
-  @Input()
-  public conveyorWorking$: BehaviorSubject<boolean>;
+  public viewBox: string;
+  public strokeDashoffset = 0;
+  public elementsInConveyor: ElemementInConveyor[] = [];
+
+  private conveyorWorkingSubscription: Subscription;
+  private readonly charOffset = 5;
 
   @Input()
   public conveyorRotation: 'left' | 'right' = 'right';
+
+  @Input()
+  public length = 200;
+
+  @Input()
+  public conveyorWorking$: BehaviorSubject<boolean>;
 
   @Input()
   public addToConveyor$: Observable<Pick<ElemementInConveyor, 'type' | 'value'>>;
@@ -20,37 +31,49 @@ export class ConveyorComponent implements OnInit {
   @Output()
   public elementDelivered = new EventEmitter<ElemementInConveyor>();
 
-  public strokeDashoffset = 0;
-  private strokeDashoffsetSubscription: Subscription;
-
-  public elementsInConveyor: ElemementInConveyor[] = [];
-
   ngOnInit(): void {
-    this.conveyorWorking$.pipe(distinctUntilChanged()).subscribe((value) => {
-      if (value) {
+    this.viewBox = `0 0 ${this.length} 85`;
+
+    this.conveyorWorking$.pipe(distinctUntilChanged()).subscribe((working) => {
+      if (working) {
         this.startConveyor();
-      } else if (this.strokeDashoffsetSubscription != null) {
+      } else if (this.conveyorWorkingSubscription != null) {
         this.stopConveyor();
       }
     });
 
-    this.addToConveyor$.pipe(filter(() => this.conveyorWorking$.getValue())).subscribe((newElement) => {
-      this.elementsInConveyor.push({ ...newElement, offset: this.conveyorRotation === 'right' ? -10 : 210 });
+    this.addToConveyor$.pipe(filter(() => this.conveyorWorking$.getValue())).subscribe((elementToAdd) => {
+      const newElement = elementToAdd as ElemementInConveyor;
+
+      newElement.startAt = newElement.startAt ?? this.conveyorRotation === 'right' ? 0 : 100;
+      newElement.removeAt = newElement.removeAt ?? this.conveyorRotation === 'right' ? 100 : 0;
+
+      const valueOffset = newElement.value.length * this.charOffset;
+      newElement.offset = this.length * (newElement.startAt / 100) + (this.conveyorRotation === 'right' ? -valueOffset : valueOffset);
+
+      this.elementsInConveyor.push(newElement);
     });
   }
 
   private startConveyor() {
-    this.strokeDashoffsetSubscription = interval(40).subscribe(() => {
+    this.conveyorWorkingSubscription = interval(40).subscribe(() => {
       this.conveyorRotation === 'right' ? this.strokeDashoffset-- : this.strokeDashoffset++;
 
       // moves the elements to the correct side
       for (let i = 0; i < this.elementsInConveyor.length; i++) {
-        this.conveyorRotation === 'right' ? this.elementsInConveyor[i].offset++ : this.elementsInConveyor[i].offset--;
-        // delete it if it is outside the viewBox
-        if (this.elementsInConveyor[i].offset > 210 || this.elementsInConveyor[i].offset < -10) {
+        const elementInConveyor = this.elementsInConveyor[i];
+        this.conveyorRotation === 'right' ? elementInConveyor.offset!++ : elementInConveyor.offset!--;
+
+        // checks if has to be removed from the list
+        const valueOffset = elementInConveyor.value.length * this.charOffset;
+        const removeAtPosition =
+          this.conveyorRotation === 'right'
+            ? this.length * (elementInConveyor.removeAt! / 100) + valueOffset
+            : this.length * (elementInConveyor.removeAt! / 100) - valueOffset;
+        if (this.conveyorRotation === 'right' ? elementInConveyor.offset > removeAtPosition : elementInConveyor.offset < removeAtPosition) {
           const [deliveredElement] = this.elementsInConveyor.splice(i, 1);
 
-          if (deliveredElement.type === 'error' || deliveredElement.type === 'complete') {
+          if (deliveredElement.type === ObservableEventType.ERROR || deliveredElement.type === ObservableEventType.COMPLETE) {
             this.stopConveyor();
           }
 
@@ -61,8 +84,7 @@ export class ConveyorComponent implements OnInit {
   }
 
   private stopConveyor() {
-    // stops the conveyor
-    this.strokeDashoffsetSubscription.unsubscribe();
+    this.conveyorWorkingSubscription.unsubscribe();
 
     this.elementsInConveyor.length = 0;
   }
---
+++ src/pages-rxjs/shared/observable-event-type.ts
@@ -0,0 +1,12 @@
+export class ObservableEventType {
+  static readonly NEXT = new ObservableEventType('NEXT', 'next', '#0a0');
+  static readonly ERROR = new ObservableEventType('ERROR', 'error', '#d00');
+  static readonly COMPLETE = new ObservableEventType('COMPLETE', 'complete', '#ffdd00');
+
+  // private to disallow creating other instances of this type
+  private constructor(private readonly key: string, public readonly name: string, public readonly color: string) {}
+
+  toString() {
+    return this.key;
+  }
+}
