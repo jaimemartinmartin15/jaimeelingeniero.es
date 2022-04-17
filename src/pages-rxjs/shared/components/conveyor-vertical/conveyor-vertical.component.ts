20fcb00ca88080717c571fc6f5ef6d6cdbbe8c64
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-17 18:17:38 +0200

    Created conveyor vertical component

---
+++ src/pages-rxjs/shared/components/conveyor-vertical/conveyor-vertical.component.ts
@@ -0,0 +1,84 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject, distinctUntilChanged, filter, interval, Observable, Subscription } from 'rxjs';
+import { ElemementInConveyor } from '../../element-in-conveyor';
+import { ObservableEventType } from '../../observable-event-type';
+
+@Component({
+  selector: 'app-conveyor-vertical',
+  templateUrl: './conveyor-vertical.component.svg',
+  styleUrls: ['./conveyor-vertical.component.scss'],
+})
+export class ConveyorVerticalComponent implements OnInit {
+  public viewBox: string;
+  public elementsInConveyor: ElemementInConveyor[] = [];
+  
+  private conveyorWorkingSubscription: Subscription;
+  public offsetLineInConveyor = 0;
+  private readonly charOffset = 25;
+  public linesInConveyor: number[];
+
+  @Input()
+  public length = 200;
+
+  @Input()
+  public conveyorWorking$: BehaviorSubject<boolean>;
+
+  @Input()
+  public addToConveyor$: Observable<Pick<ElemementInConveyor, 'type' | 'value'>>;
+
+  @Output()
+  public elementDelivered = new EventEmitter<ElemementInConveyor>();
+
+  ngOnInit(): void {
+    this.viewBox = `0 0 65 ${this.length}`;
+    this.linesInConveyor = [...Array(Math.trunc(this.length / 9.5)).keys()];
+
+    this.conveyorWorking$.pipe(distinctUntilChanged()).subscribe((working) => {
+      if (working) {
+        this.startConveyor();
+      } else if (this.conveyorWorkingSubscription != null) {
+        this.stopConveyor();
+      }
+    });
+
+    this.addToConveyor$.pipe(filter(() => this.conveyorWorking$.getValue())).subscribe((elementToAdd) => {
+      const newElement = elementToAdd as ElemementInConveyor;
+
+      newElement.startAt = 0;
+      newElement.removeAt = 100;
+      newElement.offset = -this.charOffset;
+
+      this.elementsInConveyor.push(newElement);
+    });
+  }
+
+  private startConveyor() {
+    this.conveyorWorkingSubscription = interval(40).subscribe(() => {
+      this.offsetLineInConveyor < -8 ? (this.offsetLineInConveyor = 0) : this.offsetLineInConveyor--;
+
+      // moves the elements
+      for (let i = 0; i < this.elementsInConveyor.length; i++) {
+        const elementInConveyor = this.elementsInConveyor[i];
+        elementInConveyor.offset!++;
+
+        // checks if has to be removed from the list
+        const removeAtPosition = this.length * (elementInConveyor.removeAt! / 100);
+        if (elementInConveyor.offset > removeAtPosition) {
+          const [deliveredElement] = this.elementsInConveyor.splice(i, 1);
+
+          if (deliveredElement.type === ObservableEventType.ERROR || deliveredElement.type === ObservableEventType.COMPLETE) {
+            this.stopConveyor();
+          }
+
+          this.elementDelivered.emit(deliveredElement);
+        }
+      }
+    });
+  }
+
+  private stopConveyor() {
+    this.conveyorWorkingSubscription.unsubscribe();
+
+    this.elementsInConveyor.length = 0;
+  }
+}
--- src/pages-rxjs/shared/components/rxjs-components.module.ts
+++ src/pages-rxjs/shared/components/rxjs-components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ConveyorComponent } from './conveyor/conveyor.component';
 import { ConveyorControllerComponent } from './conveyor-controller/conveyor-controller.component';
 import { SubscriberComponent } from './subscriber/subscriber.component';
+import { ConveyorVerticalComponent } from './conveyor-vertical/conveyor-vertical.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ConveyorComponent, ConveyorControllerComponent, SubscriberComponent],
-  exports: [ConveyorComponent, ConveyorControllerComponent, SubscriberComponent],
+  declarations: [ConveyorComponent, ConveyorVerticalComponent, ConveyorControllerComponent, SubscriberComponent],
+  exports: [ConveyorComponent, ConveyorVerticalComponent, ConveyorControllerComponent, SubscriberComponent],
 })
 export class RxjsComponentsModule {}
