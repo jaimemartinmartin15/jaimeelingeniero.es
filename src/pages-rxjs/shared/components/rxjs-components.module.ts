cbf9742531e82bfd22cfcdf6cf95bdea0322ed30
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-21 20:30:09 +0200

    Setup demo container component

--- src/pages-rxjs/shared/components/rxjs-components.module.ts
+++ src/pages-rxjs/shared/components/rxjs-components.module.ts
@@ -4,10 +4,11 @@ import { ConveyorComponent } from './conveyor/conveyor.component';
 import { ConveyorControllerComponent } from './conveyor-controller/conveyor-controller.component';
 import { SubscriberComponent } from './subscriber/subscriber.component';
 import { ConveyorVerticalComponent } from './conveyor-vertical/conveyor-vertical.component';
+import { DemoContainerComponent } from './demo-container/demo-container.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ConveyorComponent, ConveyorVerticalComponent, ConveyorControllerComponent, SubscriberComponent],
-  exports: [ConveyorComponent, ConveyorVerticalComponent, ConveyorControllerComponent, SubscriberComponent],
+  declarations: [ConveyorComponent, ConveyorVerticalComponent, ConveyorControllerComponent, SubscriberComponent, DemoContainerComponent],
+  exports: [ConveyorComponent, ConveyorVerticalComponent, ConveyorControllerComponent, SubscriberComponent, DemoContainerComponent],
 })
 export class RxjsComponentsModule {}
