8484ba0cc9e4438c0c72c08c1aa7d8102b03a7e6
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-04-29 19:26:01 +0200

    Setup page combineLatestWith and fix some global styles

---
+++ src/pages-rxjs/combine-latest-with/combine-latest-with.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-combine-latest-with',
+  templateUrl: './combine-latest-with.component.html',
+  styleUrls: ['./combine-latest-with.component.scss'],
+})
+export class CombineLatestWithComponent {}
