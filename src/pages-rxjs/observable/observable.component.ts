310557dbe903f85bc973842fb57a764d6c24268a
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-10 21:10:38 +0200

    Refactor observable demo

--- src/pages-rxjs/observable/observable.component.ts
+++ src/pages-rxjs/observable/observable.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { ElemementInConveyor, PickElementInConveyor } from '../shared/element-in-conveyor';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { BehaviorSubject, interval, Subject } from 'rxjs';
+import { ButtonController } from '../shared/components/conveyor-controller/button-controller';
+import { DemoContainerComponent } from '../shared/components/demo-container/demo-container.component';
+import { ElementInConveyor } from '../shared/element-in-conveyor';
 import { ObservableEventType } from '../shared/observable-event-type';
 import { SpeechBubble } from '../shared/speech-bubble';
 
@@ -9,26 +11,60 @@ import { SpeechBubble } from '../shared/speech-bubble';
   templateUrl: './observable.component.html',
   styleUrls: ['./observable.component.scss'],
 })
-export class ObservableComponent {
-  public readonly ObservableEventType = ObservableEventType;
+export class ObservableComponent implements AfterViewInit {
+  public ID = '0';
+
+  @ViewChild(DemoContainerComponent)
+  public demo: DemoContainerComponent;
+
+  public controllerButtons: ButtonController[] = [
+    { value: '🧲', type: ObservableEventType.ERROR, controllerId: this.ID, enabled: false },
+    { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: this.ID, enabled: false },
+    { value: '🍐', type: ObservableEventType.NEXT, controllerId: this.ID, enabled: false },
+    { value: '🍍', type: ObservableEventType.NEXT, controllerId: this.ID, enabled: false },
+    { value: '🍇', type: ObservableEventType.NEXT, controllerId: this.ID, enabled: false },
+  ];
 
   public conveyorWorking$ = new BehaviorSubject<boolean>(false);
-  public addToConveyor$ = new Subject<PickElementInConveyor>();
+
+  public elementsInConveyor: ElementInConveyor[] = [];
+
   public speechBubble$ = new Subject<SpeechBubble>();
+
+  public ngAfterViewInit(): void {
+    interval(this.demo.fps).subscribe(() => {
+      this.elementsInConveyor.forEach((e) => {
+        e.x += this.demo.speed;
+        if (e.x >= 440) {
+          this.elementsInConveyor.splice(this.elementsInConveyor.indexOf(e), 1);
+          this.speechBubble$.next({
+            type: e.type,
+            message: e.value,
+          });
+          if (e.type === ObservableEventType.ERROR || e.type === ObservableEventType.COMPLETE) {
+            this.onSubscribe(false);
+          }
+        }
+      });
+    });
+  }
 
   public onSubscribe(isSubscribed: boolean) {
     this.conveyorWorking$.next(isSubscribed);
+    this.controllerButtons.forEach((b) => (b.enabled = isSubscribed));
   }
 
-  public onControllerButtonClick(buttonElement: PickElementInConveyor) {
-    this.addToConveyor$.next(buttonElement);
-  }
+  public onControllerButtonClick(button: ButtonController) {
+    if (button.type === ObservableEventType.ERROR || button.type === ObservableEventType.COMPLETE) {
+      this.controllerButtons.forEach((b) => (b.enabled = false));
+    }
 
-  public onEventDelivered(element: ElemementInConveyor) {
-    this.speechBubble$.next({
-      message: element.value,
-      type: element.type,
+    this.elementsInConveyor.push({
+      type: button.type,
+      value: button.value,
+      x: 210,
+      y: 156,
+      conveyorId: button.controllerId,
     });
-    this.conveyorWorking$.next(element.type === ObservableEventType.NEXT);
   }
 }
