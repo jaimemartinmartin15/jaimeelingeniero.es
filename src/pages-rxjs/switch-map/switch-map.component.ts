9819f8220a7442e51b878d3ffce14d4fd573c0a5
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-13 19:26:37 +0200

    Animations switch map demo

--- src/pages-rxjs/switch-map/switch-map.component.ts
+++ src/pages-rxjs/switch-map/switch-map.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { BehaviorSubject, interval, Subject } from 'rxjs';
-import { animationConveyor } from '../shared/rxjs-animations';
+import { fadeInOut } from '../shared/rxjs-animations';
 import { ElementInConveyor } from '../shared/element-in-conveyor';
 import { ObservableEventType } from '../shared/observable-event-type';
 import { SpeechBubble } from '../shared/speech-bubble';
@@ -11,7 +11,7 @@ import { DemoContainerComponent } from '../shared/components/demo-container/demo
   selector: 'app-switch-map',
   templateUrl: './switch-map.component.html',
   styleUrls: ['./switch-map.component.scss'],
-  animations: [animationConveyor],
+  animations: [fadeInOut],
 })
 export class SwitchMapComponent implements AfterViewInit {
   private nextSwitchMapId = 2;
@@ -27,14 +27,14 @@ export class SwitchMapComponent implements AfterViewInit {
 
   public elementsInConveyor: ElementInConveyor[] = [];
 
-  private readonly initialPositions: { [key: string]: { x: number; y: number } } = {
-    [this.MAIN_O]: { x: 260, y: 546 },
-    [this.MAIN_S]: { x: 466, y: 546 },
+  private readonly initialPositions: { [key: string]: { x?: number; y?: number } } = {
+    [this.MAIN_O]: { x: 260 },
+    [this.MAIN_S]: { x: 466 },
   };
 
-  private readonly finalPositions: { [key: string]: { x: number; y: number } } = {
-    [this.MAIN_O]: { x: 420, y: 546 },
-    [this.MAIN_S]: { x: 645, y: 546 },
+  private readonly finalPositions: { [key: string]: { x?: number; y?: number } } = {
+    [this.MAIN_O]: { x: 420 },
+    [this.MAIN_S]: { x: 645 },
   };
 
   public controllerButtons: { [key: string]: ButtonController[] } = {
@@ -73,8 +73,7 @@ export class SwitchMapComponent implements AfterViewInit {
         type: e.type,
         value: e.value,
         x: e.x,
-        y: e.y,
-      });
+      } as ElementInConveyor);
     } else if (e.conveyorId === this.MAIN_S && e.type === ObservableEventType.NEXT) {
       this.speechBubble$.next({
         message: e.value,
@@ -95,18 +94,21 @@ export class SwitchMapComponent implements AfterViewInit {
         conveyorId: this.MAIN_S,
         type: e.type,
         value: e.value,
-        ...this.initialPositions[this.MAIN_S],
-      });
+        x: this.initialPositions[this.MAIN_S].x,
+      } as ElementInConveyor);
     } else {
       this.elementsInConveyor.push({
         conveyorId: this.MAIN_S,
         type: e.type,
         value: e.value,
-        ...this.initialPositions[this.MAIN_S],
-      });
+        x: this.initialPositions[this.MAIN_S].x,
+      } as ElementInConveyor);
 
       this.controllerButtons[this.MAIN_O].forEach(
-        (button) => (button.enabled = this.elementsInConveyor.every((ec) => e.conveyorId !== ec.conveyorId))
+        (button) =>
+          (button.enabled = !this.elementsInConveyor
+            .filter((ec) => ec.conveyorId === e.conveyorId)
+            .some((ec) => ec.type !== ObservableEventType.COMPLETE))
       );
     }
   }
@@ -126,13 +128,11 @@ export class SwitchMapComponent implements AfterViewInit {
 
     this.conveyorsWorking[S_ID] = new BehaviorSubject<boolean>(true);
 
-    this.initialPositions[S_ID] = { x: 450 - 100 * this.SWITCH.length + 50, y: 200 };
-    this.finalPositions[S_ID] = { x: 450 - 100 * this.SWITCH.length + 50, y: 405 };
+    this.initialPositions[S_ID] = { y: 120 };
+    this.finalPositions[S_ID] = { y: 325 };
 
     if (this.SWITCH.length == 2) {
       this.removeSwitchMapConveyor(this.SWITCH[0]);
-    } else {
-      this.recalculatePositionOfElementsInConveyor();
     }
   }
 
@@ -145,30 +145,6 @@ export class SwitchMapComponent implements AfterViewInit {
     delete this.finalPositions[id];
 
     this.elementsInConveyor = this.elementsInConveyor.filter((e) => e.conveyorId !== id);
-
-    this.recalculatePositionOfElementsInConveyor();
-  }
-
-  private recalculatePositionOfElementsInConveyor() {
-    Object.keys(this.initialPositions)
-      .slice(2)
-      .forEach((key, index) => {
-        this.initialPositions[key].x = 450 - 100 * (this.SWITCH.length - 1) + 200 * index;
-        this.initialPositions[key].y = 200;
-      });
-
-    Object.keys(this.finalPositions)
-      .slice(2)
-      .forEach((key, index) => {
-        this.finalPositions[key].x = 450 - 100 * (this.SWITCH.length - 1) + 200 * index;
-        this.finalPositions[key].y = 405;
-      });
-
-    this.elementsInConveyor.forEach((e) => {
-      if (e.conveyorId !== this.MAIN_O && e.conveyorId !== this.MAIN_S) {
-        e.x = 450 - 100 * (this.SWITCH.length - 1) + 200 * this.SWITCH.indexOf(e.conveyorId);
-      }
-    });
   }
 
   private moveElementInConveyor(e: ElementInConveyor): boolean {
@@ -177,13 +153,13 @@ export class SwitchMapComponent implements AfterViewInit {
       e.x += this.demo.speed;
 
       if (e.conveyorId === this.MAIN_O) {
-        isOutside = e.x >= this.finalPositions[this.MAIN_O].x;
+        isOutside = e.x >= this.finalPositions[this.MAIN_O].x!;
       } else {
-        isOutside = e.x >= this.finalPositions[this.MAIN_S].x;
+        isOutside = e.x >= this.finalPositions[this.MAIN_S].x!;
       }
     } else {
       e.y += this.demo.speed;
-      isOutside = e.y >= this.finalPositions[e.conveyorId].y;
+      isOutside = e.y >= this.finalPositions[e.conveyorId].y!;
     }
 
     return isOutside;
@@ -228,6 +204,6 @@ export class SwitchMapComponent implements AfterViewInit {
       value: button.value,
       ...this.initialPositions[button.controllerId],
       conveyorId: button.controllerId,
-    });
+    } as ElementInConveyor);
   }
 }
