0c294109faa0468041d26362c81f26981693ee14
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-13 17:58:14 +0200

    Fade in out animation

--- src/pages-rxjs/shared/rxjs-animations.ts
+++ src/pages-rxjs/shared/rxjs-animations.ts
@@ -1,11 +1,6 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 
-export const animationConveyor = trigger('animationConveyor', [
-  transition(':enter', [
-    style({ opacity: 0,  }),
-    animate('500ms', style({ opacity: '1' }))
-  ]),
-  transition(':leave', [
-    style({ opacity: 1 }),
-    animate('500ms', style({ opacity: '0' }))]),
+export const fadeInOut = trigger('fadeInOut', [
+  transition(':enter', [style({ opacity: 0 }), animate('300ms', style({ opacity: '1' }))]),
+  transition(':leave', [style({ opacity: 1 }), animate('300ms', style({ opacity: '0' }))]),
 ]);
