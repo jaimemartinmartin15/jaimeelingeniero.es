faaf6c62488c6974ea6d538a6b93db8b899dff90
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-13 15:50:20 +0200

    Implement switch map demo

--- src/pages-rxjs/switch-map/switch-map.component.ts
+++ src/pages-rxjs/switch-map/switch-map.component.ts
@@ -13,11 +13,11 @@ import { DemoContainerComponent } from '../shared/components/demo-container/demo
   styleUrls: ['./switch-map.component.scss'],
   animations: [observableAnimation],
 })
-export class SwitchMapComponent {
-  private nextMergeMapId = 2;
+export class SwitchMapComponent implements AfterViewInit {
+  private nextSwitchMapId = 2;
 
-  public readonly MAIN_M = '0';
-  public readonly MAIN_E = '1';
+  public readonly MAIN_O = '0'; // Main deliver to operator
+  public readonly MAIN_S = '1'; // Main deliver to subscriber
   public readonly SWITCH: string[] = [];
 
   @ViewChild(DemoContainerComponent)
@@ -28,27 +28,27 @@ export class SwitchMapComponent {
   public elementsInConveyor: ElementInConveyor[] = [];
 
   private readonly initialPositions: { [key: string]: { x: number; y: number } } = {
-    [this.MAIN_M]: { x: 260, y: 546 },
-    [this.MAIN_E]: { x: 466, y: 546 },
+    [this.MAIN_O]: { x: 260, y: 546 },
+    [this.MAIN_S]: { x: 466, y: 546 },
   };
 
   private readonly finalPositions: { [key: string]: { x: number; y: number } } = {
-    [this.MAIN_M]: { x: 420, y: 546 },
-    [this.MAIN_E]: { x: 645, y: 546 },
+    [this.MAIN_O]: { x: 420, y: 546 },
+    [this.MAIN_S]: { x: 645, y: 546 },
   };
 
   public controllerButtons: { [key: string]: ButtonController[] } = {
-    [this.MAIN_M]: [
-      { value: '🏠', type: ObservableEventType.ERROR, controllerId: this.MAIN_M, enabled: false },
-      { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: this.MAIN_M, enabled: false },
-      { value: '🍎', type: ObservableEventType.NEXT, controllerId: this.MAIN_M, enabled: false },
-      { value: '🍌', type: ObservableEventType.NEXT, controllerId: this.MAIN_M, enabled: false },
-      { value: '🥝', type: ObservableEventType.NEXT, controllerId: this.MAIN_M, enabled: false },
+    [this.MAIN_O]: [
+      { value: '🏠', type: ObservableEventType.ERROR, controllerId: this.MAIN_O, enabled: false },
+      { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: this.MAIN_O, enabled: false },
+      { value: '🍎', type: ObservableEventType.NEXT, controllerId: this.MAIN_O, enabled: false },
+      { value: '🍌', type: ObservableEventType.NEXT, controllerId: this.MAIN_O, enabled: false },
+      { value: '🥝', type: ObservableEventType.NEXT, controllerId: this.MAIN_O, enabled: false },
     ],
   };
 
   public conveyorsWorking: { [key: string]: BehaviorSubject<boolean> } = {
-    [this.MAIN_M]: new BehaviorSubject<boolean>(false),
+    [this.MAIN_O]: new BehaviorSubject<boolean>(false),
   };
 
   public ngAfterViewInit(): void {
@@ -65,14 +65,22 @@ export class SwitchMapComponent {
   }
 
   private handleDeliveredElement(e: ElementInConveyor) {
-    if (e.conveyorId === this.MAIN_M && e.type === ObservableEventType.NEXT) {
-      this.addNewSwitchMapConveyor();
-    } else if (e.conveyorId === this.MAIN_E && e.type === ObservableEventType.NEXT) {
+    if (e.conveyorId === this.MAIN_O && e.type === ObservableEventType.NEXT) {
+      this.replaceSwitchMapConveyor();
+    } else if (e.conveyorId === this.MAIN_O) {
+      this.elementsInConveyor.push({
+        conveyorId: this.MAIN_S,
+        type: e.type,
+        value: e.value,
+        x: e.x,
+        y: e.y,
+      });
+    } else if (e.conveyorId === this.MAIN_S && e.type === ObservableEventType.NEXT) {
       this.speechBubble$.next({
         message: e.value,
         type: e.type,
       });
-    } else if (e.conveyorId === this.MAIN_M || e.conveyorId === this.MAIN_E) {
+    } else if (e.conveyorId === this.MAIN_S) {
       this.speechBubble$.next({
         message: e.value,
         type: e.type,
@@ -80,13 +88,51 @@ export class SwitchMapComponent {
       this.onSubscribe(false);
     } else if (e.type === ObservableEventType.COMPLETE) {
       this.removeSwitchMapConveyor(e.conveyorId);
-    } else {
+      this.controllerButtons[this.MAIN_O].forEach((button) => (button.enabled = true));
+    } else if (e.type === ObservableEventType.ERROR) {
+      this.conveyorsWorking[e.conveyorId].next(false);
       this.elementsInConveyor.push({
-        conveyorId: this.MAIN_E,
+        conveyorId: this.MAIN_S,
         type: e.type,
         value: e.value,
-        ...this.initialPositions[this.MAIN_E],
+        ...this.initialPositions[this.MAIN_S],
       });
+    } else {
+      this.elementsInConveyor.push({
+        conveyorId: this.MAIN_S,
+        type: e.type,
+        value: e.value,
+        ...this.initialPositions[this.MAIN_S],
+      });
+
+      this.controllerButtons[this.MAIN_O].forEach(
+        (button) => (button.enabled = this.elementsInConveyor.every((ec) => e.conveyorId !== ec.conveyorId))
+      );
+    }
+  }
+
+  private replaceSwitchMapConveyor() {
+    const S_ID = `${this.nextSwitchMapId++}`;
+    this.SWITCH.push(S_ID);
+
+    const buttonsEnabled = this.controllerButtons[this.MAIN_O][0].enabled && this.elementsInConveyor.every((e) => e.conveyorId != this.MAIN_O);
+    this.controllerButtons[S_ID] = [
+      { value: '🏠', type: ObservableEventType.ERROR, controllerId: S_ID, enabled: buttonsEnabled },
+      { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: S_ID, enabled: buttonsEnabled },
+      { value: '🍎', type: ObservableEventType.NEXT, controllerId: S_ID, enabled: buttonsEnabled },
+      { value: '🍌', type: ObservableEventType.NEXT, controllerId: S_ID, enabled: buttonsEnabled },
+      { value: '🥝', type: ObservableEventType.NEXT, controllerId: S_ID, enabled: buttonsEnabled },
+    ];
+
+    this.conveyorsWorking[S_ID] = new BehaviorSubject<boolean>(true);
+
+    this.initialPositions[S_ID] = { x: 450 - 100 * this.SWITCH.length + 50, y: 200 };
+    this.finalPositions[S_ID] = { x: 450 - 100 * this.SWITCH.length + 50, y: 405 };
+
+    if (this.SWITCH.length == 2) {
+      this.removeSwitchMapConveyor(this.SWITCH[0]);
+    } else {
+      this.recalculatePositionOfElementsInConveyor();
     }
   }
 
@@ -98,6 +144,8 @@ export class SwitchMapComponent {
     delete this.initialPositions[id];
     delete this.finalPositions[id];
 
+    this.elementsInConveyor = this.elementsInConveyor.filter((e) => e.conveyorId !== id);
+
     this.recalculatePositionOfElementsInConveyor();
   }
 
@@ -105,56 +153,33 @@ export class SwitchMapComponent {
     Object.keys(this.initialPositions)
       .slice(2)
       .forEach((key, index) => {
-        this.initialPositions[key].x = 450 - 100 * (this.SWITCH.length - 1) + index * 200;
+        this.initialPositions[key].x = 450 - 100 * (this.SWITCH.length - 1) + 200 * index;
         this.initialPositions[key].y = 200;
       });
 
     Object.keys(this.finalPositions)
       .slice(2)
       .forEach((key, index) => {
-        this.finalPositions[key].x = 450 - 100 * (this.SWITCH.length - 1) + index * 200;
+        this.finalPositions[key].x = 450 - 100 * (this.SWITCH.length - 1) + 200 * index;
         this.finalPositions[key].y = 405;
       });
 
-    this.elementsInConveyor.forEach((e, index) => {
-      if (e.conveyorId !== this.MAIN_M && e.conveyorId !== this.MAIN_E) {
-        e.x = 450 - 100 * (this.SWITCH.length - 1) + index * 200;
+    this.elementsInConveyor.forEach((e) => {
+      if (e.conveyorId !== this.MAIN_O && e.conveyorId !== this.MAIN_S) {
+        e.x = 450 - 100 * (this.SWITCH.length - 1) + 200 * this.SWITCH.indexOf(e.conveyorId);
       }
     });
   }
 
-  private addNewSwitchMapConveyor() {
-    const M_ID = `${this.nextMergeMapId++}`;
-    this.SWITCH.push(M_ID);
-
-    this.controllerButtons[M_ID] = [
-      { value: '🏠', type: ObservableEventType.ERROR, controllerId: M_ID, enabled: true },
-      { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: M_ID, enabled: true },
-      { value: '🍎', type: ObservableEventType.NEXT, controllerId: M_ID, enabled: true },
-      { value: '🍌', type: ObservableEventType.NEXT, controllerId: M_ID, enabled: true },
-      { value: '🥝', type: ObservableEventType.NEXT, controllerId: M_ID, enabled: true },
-    ];
-
-    this.conveyorsWorking[M_ID] = new BehaviorSubject<boolean>(true);
-
-    this.initialPositions[M_ID] = { x: 450 - 100 * (this.SWITCH.length - 1) + (parseInt(M_ID) - 2) * 200, y: 200 };
-    this.finalPositions[M_ID] = { x: 450 - 100 * (this.SWITCH.length - 1) + (parseInt(M_ID) - 2) * 200, y: 405 };
-
-    this.recalculatePositionOfElementsInConveyor();
-  }
-
   private moveElementInConveyor(e: ElementInConveyor): boolean {
     let isOutside = false;
-    if (e.conveyorId === this.MAIN_M || e.conveyorId === this.MAIN_E) {
+    if (e.conveyorId === this.MAIN_O || e.conveyorId === this.MAIN_S) {
       e.x += this.demo.speed;
-      if (e.conveyorId === this.MAIN_M) {
-        isOutside = e.x >= this.finalPositions[this.MAIN_M].x;
-      } else {
-        isOutside = e.x >= this.finalPositions[this.MAIN_E].x;
-      }
 
-      if (e.type !== ObservableEventType.NEXT) {
-        isOutside = e.x >= this.finalPositions[this.MAIN_E].x;
+      if (e.conveyorId === this.MAIN_O) {
+        isOutside = e.x >= this.finalPositions[this.MAIN_O].x;
+      } else {
+        isOutside = e.x >= this.finalPositions[this.MAIN_S].x;
       }
     } else {
       e.y += this.demo.speed;
@@ -168,19 +193,34 @@ export class SwitchMapComponent {
     Object.values(this.controllerButtons).forEach((controller) => controller.forEach((button) => (button.enabled = isSubscribed)));
     Object.values(this.conveyorsWorking).forEach((conveyor) => conveyor.next(isSubscribed));
 
+    this.SWITCH.forEach((id) => {
+      delete this.conveyorsWorking[id];
+      delete this.controllerButtons[id];
+      delete this.initialPositions[id];
+      delete this.finalPositions[id];
+    });
     this.SWITCH.length = 0;
     this.elementsInConveyor.length = 0;
   }
 
   public onControllerButtonClick(button: ButtonController) {
     if (button.type === ObservableEventType.ERROR) {
+      // disable all buttons of all controllers
       Object.values(this.controllerButtons).forEach((controller) => controller.forEach((button) => (button.enabled = false)));
     } else if (button.type === ObservableEventType.COMPLETE) {
-      if (button.controllerId === this.MAIN_M) {
+      if (button.controllerId === this.MAIN_O) {
+        // disable all buttons of all controllers
         Object.values(this.controllerButtons).forEach((controller) => controller.forEach((button) => (button.enabled = false)));
       } else {
+        // disable all buttons of the switch map controller
         this.controllerButtons[button.controllerId].forEach((button) => (button.enabled = false));
       }
+    } else if (button.controllerId == this.MAIN_O && this.SWITCH.length > 0) {
+      // disable all buttons of the switch map controller
+      this.controllerButtons[this.SWITCH[0]].forEach((button) => (button.enabled = false));
+    } else if (button.controllerId != this.MAIN_O) {
+      // disable all buttons of the main controller
+      this.controllerButtons[this.MAIN_O].forEach((button) => (button.enabled = false));
     }
 
     this.elementsInConveyor.push({
