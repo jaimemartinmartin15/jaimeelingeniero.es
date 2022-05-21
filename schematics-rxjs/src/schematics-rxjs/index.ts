084e6ef1b506427d77ae80f9bcc9906354117fb1
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-21 13:50:02 +0200

    Added file templates and implement schematic function

--- schematics-rxjs/src/schematics-rxjs/index.ts
+++ schematics-rxjs/src/schematics-rxjs/index.ts
@@ -1,10 +1,20 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, mergeWith, Rule, SchematicContext, Tree, url, template } from '@angular-devkit/schematics';
 
+import { strings } from '@angular-devkit/core';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function schematicsRxjs(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+import { Schema } from './schema';
+
+export function schematicsRxjs({ name }: Schema): Rule {
+  return (_: Tree, _context: SchematicContext) => {
+    const sourceTemplates = url('./files');
+
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        name,
+        ...strings,
+      }),
+    ]);
+
+    return mergeWith(sourceParametrizedTemplates);
   };
 }
