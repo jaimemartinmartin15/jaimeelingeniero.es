fd458db0c39ac748500dec6ae40777a17eacef13
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-26 20:04:50 +0200

    Added html home rxjs page

--- src/pages-rxjs/home/home.component.ts
+++ src/pages-rxjs/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HOME_LINKS } from './home-links';
 
 @Component({
   selector: 'app-rxjs-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public HOME_LINKS = HOME_LINKS;
+}
