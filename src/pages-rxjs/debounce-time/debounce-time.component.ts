a03af86720470ab6914e013b10d0703852ad42bb
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-05-29 18:26:14 +0200

    Setup debounce time page

---
+++ src/pages-rxjs/debounce-time/debounce-time.component.ts
@@ -0,0 +1,10 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debounce-time',
+  templateUrl: './debounce-time.component.html',
+  styleUrls: ['./debounce-time.component.scss'],
+})
+export class DebounceTimeComponent implements AfterViewInit {
+  public ngAfterViewInit() {}
+}
