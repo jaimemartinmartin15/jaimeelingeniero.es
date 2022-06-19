d74a1cc85fc5d73f42b038da768fa45c3c76de80
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-06-19 16:33:52 +0200

    Update component template schematic rxjs-page

--- app-schematics/src/rxjs-page/files/__name@dasherize__/__name@dasherize__.component.ts
+++ app-schematics/src/rxjs-page/files/__name@dasherize__/__name@dasherize__.component.ts
@@ -1,10 +1,23 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-<%= dasherize(name) %>',
   templateUrl: './<%= dasherize(name) %>.component.html',
   styleUrls: ['./<%= dasherize(name) %>.component.scss'],
 })
-export class <%= classify(name) %>Component implements AfterViewInit {
+export class <%= classify(name) %>Component implements OnInit, AfterViewInit, OnDestroy {
+
+  public constructor(private readonly titleService: Title, private readonly metaService: Meta) {}
+
+  public ngOnInit() {
+    this.titleService.setTitle('<%= classify(name) %> rxjs');
+    this.metaService.updateTag({ name: 'description', content: 'Explicación del operador rxjs <%= name %>' });
+  }
+
   public ngAfterViewInit() {}
+
+  public ngOnDestroy(): void {
+    this.metaService.removeTag('name="description"');
+  }
 }
