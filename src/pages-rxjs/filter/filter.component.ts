a1fb4bd13efee3b327769ab741e3e9032ea8a507
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-06-19 12:52:57 +0200

    Added demo filter operator

--- src/pages-rxjs/filter/filter.component.ts
+++ src/pages-rxjs/filter/filter.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { BehaviorSubject, interval, Subject } from 'rxjs';
+import { ButtonController } from '../shared/components/conveyor-controller/button-controller';
+import { DemoContainerComponent } from '../shared/components/demo-container/demo-container.component';
+import { ElementInConveyor } from '../shared/element-in-conveyor';
+import { ObservableEventType } from '../shared/observable-event-type';
+import { SpeechBubble } from '../shared/speech-bubble';
 
 @Component({
   selector: 'app-filter',
@@ -6,5 +12,71 @@ import { AfterViewInit, Component } from '@angular/core';
   styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements AfterViewInit {
-  public ngAfterViewInit() {}
+  public ID = '0';
+
+  @ViewChild(DemoContainerComponent)
+  public demo: DemoContainerComponent;
+
+  public controllerButtons: ButtonController[] = [
+    { value: '⚽', type: ObservableEventType.ERROR, controllerId: this.ID, enabled: false },
+    { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: this.ID, enabled: false },
+    { value: '🥦', type: ObservableEventType.NEXT, controllerId: this.ID, enabled: false },
+    { value: '🍌', type: ObservableEventType.NEXT, controllerId: this.ID, enabled: false },
+    { value: '🍇', type: ObservableEventType.NEXT, controllerId: this.ID, enabled: false },
+  ];
+
+  public conveyorWorking$ = new BehaviorSubject<boolean>(false);
+
+  public elementsInConveyor: ElementInConveyor[] = [];
+
+  public speechBubble$ = new Subject<SpeechBubble>();
+
+  public ngAfterViewInit() {
+    interval(this.demo.fps).subscribe(() => {
+      this.elementsInConveyor.forEach((e) => {
+        e.x += this.demo.speed;
+
+        if (e.x >= 300 && e.x < 320 && e.type === ObservableEventType.NEXT) {
+          this._nextElementReachesOperator(e);
+        } else if (e.x >= 450) {
+          this.elementsInConveyor.splice(this.elementsInConveyor.indexOf(e), 1);
+          this.speechBubble$.next({
+            type: e.type,
+            message: e.value,
+          });
+
+          if (e.type === ObservableEventType.COMPLETE || e.type === ObservableEventType.ERROR) {
+            this.onSubscribe(false);
+          }
+        }
+      });
+    });
+  }
+
+  private _nextElementReachesOperator(e: ElementInConveyor) {
+    if (e.value === '🥦') {
+      e.x = 350;
+    } else {
+      this.elementsInConveyor.splice(this.elementsInConveyor.indexOf(e), 1);
+    }
+  }
+
+  public onSubscribe(isSubscribed: boolean) {
+    this.conveyorWorking$.next(isSubscribed);
+    this.controllerButtons.forEach((b) => (b.enabled = isSubscribed));
+    this.elementsInConveyor.length = 0;
+  }
+
+  public onControllerButtonClick(button: ButtonController) {
+    if (button.type === ObservableEventType.ERROR || button.type === ObservableEventType.COMPLETE) {
+      this.controllerButtons.forEach((b) => (b.enabled = false));
+    }
+
+    this.elementsInConveyor.push({
+      type: button.type,
+      value: button.value,
+      x: 220,
+      conveyorId: button.controllerId,
+    } as ElementInConveyor);
+  }
 }
