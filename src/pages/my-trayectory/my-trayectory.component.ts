91d78f7f9d4cba49ba3e5ee3eaf16f87b9cb2466
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-06-19 14:07:06 +0200

    Update head tags in my-trayectory page

--- src/pages/my-trayectory/my-trayectory.component.ts
+++ src/pages/my-trayectory/my-trayectory.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-my-trayectory',
   templateUrl: './my-trayectory.component.html',
-  styleUrls: ['./my-trayectory.component.scss']
+  styleUrls: ['./my-trayectory.component.scss'],
 })
 export class MyTrayectoryComponent {
+  public constructor(private readonly titleService: Title, private readonly metaService: Meta) {}
+
+  public ngOnInit() {
+    this.titleService.setTitle('Trayectoria');
+    this.metaService.updateTag({ name: 'description', content: 'Esta es mi carrera profesional hasta hoy' });
+    this.metaService.removeTag('name="keywords"');
+  }
 }
