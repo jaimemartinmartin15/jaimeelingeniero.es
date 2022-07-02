df24dfcaaf2d3774f35b2283600a93e929b28b91
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-07-02 20:14:20 +0200

    Remove top navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { TopNavigationSvgModule } from 'src/svg/generated/top-navigation-svg.mod
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, TopNavigationComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, TopNavigationSvgModule],
   bootstrap: [AppComponent],
 })
