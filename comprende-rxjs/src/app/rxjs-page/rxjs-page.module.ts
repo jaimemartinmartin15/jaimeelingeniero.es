94a01c37eee3727b152171dd8827ebf2c9704609
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-07-16 19:03:03 +0200

    Add highlight links in index.html

--- comprende-rxjs/src/app/rxjs-page/rxjs-page.module.ts
+++ comprende-rxjs/src/app/rxjs-page/rxjs-page.module.ts
@@ -1,5 +1,5 @@
-import { Inject, NgModule } from '@angular/core';
-import { CommonModule, DOCUMENT } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RxjsPageComponent } from './rxjs-page.component';
 import { RxjsPageRoutingModule } from './rxjs-page-routing.module';
 
@@ -7,22 +7,4 @@ import { RxjsPageRoutingModule } from './rxjs-page-routing.module';
   imports: [CommonModule, RxjsPageRoutingModule],
   declarations: [RxjsPageComponent],
 })
-export class PagesRxjsModule {
-  public constructor(@Inject(DOCUMENT) private document: Document) {
-    const head = this.document.getElementsByTagName('head')[0];
-
-    // loads style for syntax highlight
-    const highlightStyle = this.document.createElement('link');
-    highlightStyle.rel = 'stylesheet';
-    highlightStyle.href =
-      'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/default.min.css';
-    head.appendChild(highlightStyle);
-
-    // loads script for syntx highlight
-    const highlightScript = this.document.createElement('script');
-    highlightScript.type = 'application/javascript';
-    highlightScript.src =
-      'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/highlight.min.js';
-    head.appendChild(highlightScript);
-  }
-}
+export class PagesRxjsModule {}
