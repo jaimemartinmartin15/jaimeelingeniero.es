94fe54add964b34530d8625c4fe5f85ac1720890
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-07-20 19:39:51 +0200

    Show list of groups in lateral menu rxjs. Redirect to home menu on non-desktop devices

--- src/pages-rxjs/pages-rxjs.component.ts
+++ src/pages-rxjs/pages-rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HOME_LINKS_OTHERS, HOME_LINKS_TO_START } from './home/home-links';
 
 @Component({
   selector: 'app-pages-rxjs',
@@ -6,17 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pages-rxjs.component.scss'],
 })
 export class PagesRxjsComponent implements OnInit {
+  public readonly GROUPS_OF_OPERATORS = HOME_LINKS_OTHERS;
+  public readonly GROUP_TO_START = HOME_LINKS_TO_START;
+
   public isSmallerThanDesktop: boolean = window.matchMedia('(max-width: 1200px)').matches;
-  public linksCollapsed: boolean = window.matchMedia('(max-width: 1200px)').matches;
 
   public ngOnInit() {
     window.matchMedia('(max-width: 1200px)').addEventListener('change', (e) => {
       this.isSmallerThanDesktop = e.matches;
-      this.linksCollapsed = e.matches;
     });
-  }
-
-  public collapse() {
-    this.linksCollapsed = !this.linksCollapsed;
   }
 }
