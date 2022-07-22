ec2bf3768ad8ab6316c2b80bf0ae58bb06e5fda9
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-07-22 19:02:41 +0200

    Added demo BehaviorSubject

--- src/pages-rxjs/behavior-subject/behavior-subject.component.ts
+++ src/pages-rxjs/behavior-subject/behavior-subject.component.ts
@@ -1,8 +1,113 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { BehaviorSubject, interval, Subject } from 'rxjs';
+import { ButtonController } from '../shared/components/conveyor-controller/button-controller';
+import { DemoContainerComponent } from '../shared/components/demo-container/demo-container.component';
+import { ElementInConveyor } from '../shared/element-in-conveyor';
+import { ObservableEventType } from '../shared/observable-event-type';
+import { SpeechBubble } from '../shared/speech-bubble';
 
 @Component({
   selector: 'app-behavior-subject',
   templateUrl: './behavior-subject.component.html',
   styleUrls: ['./behavior-subject.component.scss'],
 })
-export class BehaviorSubjectComponent {}
+export class BehaviorSubjectComponent implements OnInit, AfterViewInit {
+  public S1 = '0';
+  public S2 = '1';
+  public S3 = '2';
+
+  @ViewChild(DemoContainerComponent)
+  public demo: DemoContainerComponent;
+
+  public controllerButtons: ButtonController[] = [
+    { value: '🧲', type: ObservableEventType.ERROR, controllerId: '', enabled: true },
+    { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: '', enabled: true },
+    { value: '🍐', type: ObservableEventType.NEXT, controllerId: '', enabled: true },
+    { value: '🍍', type: ObservableEventType.NEXT, controllerId: '', enabled: true },
+    { value: '🍇', type: ObservableEventType.NEXT, controllerId: '', enabled: true },
+  ];
+
+  public conveyorWorking$ = new BehaviorSubject<boolean>(true);
+
+  public elementInStandBy = '🥝';
+  public elementsInConveyor: ElementInConveyor[] = [];
+
+  public subscriptionsStatus = {
+    [this.S1]: false,
+    [this.S2]: false,
+    [this.S3]: false,
+  };
+
+  public speechBubble$ = {
+    [this.S1]: new Subject<SpeechBubble>(),
+    [this.S2]: new Subject<SpeechBubble>(),
+    [this.S3]: new Subject<SpeechBubble>(),
+  };
+
+  public constructor(private readonly titleService: Title, private readonly metaService: Meta) {}
+
+  public ngOnInit() {
+    this.titleService.setTitle('Subject rxjs');
+    this.metaService.updateTag({ name: 'description', content: 'Explicación de un Subject' });
+  }
+
+  public ngAfterViewInit(): void {
+    interval(this.demo.fps).subscribe(() => {
+      this.elementsInConveyor.forEach((e) => {
+        e.y += this.demo.speed;
+        if (e.y >= 440) {
+          if (e.type === ObservableEventType.NEXT) {
+            this.elementInStandBy = e.value;
+          }
+          this.elementsInConveyor.splice(this.elementsInConveyor.indexOf(e), 1);
+          Object.values(this.speechBubble$).forEach((speechBubble, index) => {
+            // S1, S2 or S3
+            if (this.subscriptionsStatus[`${index}`]) {
+              speechBubble.next({
+                type: e.type,
+                message: e.value,
+              });
+            }
+          });
+          if (e.type === ObservableEventType.ERROR || e.type === ObservableEventType.COMPLETE) {
+            this.controllerButtons.forEach((button) => (button.enabled = false));
+            this.conveyorWorking$.next(false);
+            this.elementsInConveyor.length = 0;
+            Object.keys(this.subscriptionsStatus).forEach((key) => (this.subscriptionsStatus[key] = false));
+          }
+        }
+      });
+    });
+  }
+
+  public onSubscribe(subscriberId: string, isSubscribed: boolean) {
+    if (!this.conveyorWorking$.getValue()) {
+      this.elementInStandBy = '🥝';
+    }
+    this.subscriptionsStatus[subscriberId] = isSubscribed;
+    this.conveyorWorking$.next(true);
+    this.controllerButtons.forEach((button) => (button.enabled = true));
+
+    if (isSubscribed) {
+      this.speechBubble$[subscriberId].next({
+        message: this.elementInStandBy,
+        type: ObservableEventType.NEXT,
+      });
+    }
+  }
+
+  public onControllerButtonClick(button: ButtonController) {
+    this.elementsInConveyor.push({
+      type: button.type,
+      value: button.value,
+      x: 370,
+      y: 170,
+      conveyorId: button.controllerId,
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.metaService.removeTag('name="description"');
+  }
+}
