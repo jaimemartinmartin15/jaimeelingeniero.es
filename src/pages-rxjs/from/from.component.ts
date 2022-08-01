42e342340161414fddd853ff5c8e8c506c6a1702
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-01 16:48:14 +0200

    Clean up from rxjs page because it will not include demo

--- src/pages-rxjs/from/from.component.ts
+++ src/pages-rxjs/from/from.component.ts
@@ -1,84 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { BehaviorSubject, Subject, from } from 'rxjs';
-import { ElementInConveyor } from '../shared/element-in-conveyor';
-import { ObservableEventType } from '../shared/observable-event-type';
+import { BehaviorSubject, from } from 'rxjs';
 import { ButtonController } from '../shared/components/conveyor-controller/button-controller';
-import { BaseOperatorComponent } from '../shared/base-operator.component';
 
 @Component({
   selector: 'app-from',
   templateUrl: './from.component.html',
   styleUrls: ['./from.component.scss'],
 })
-export class FromComponent extends BaseOperatorComponent {
-  protected operator = from('🥚🥕🥩');
+export class FromComponent implements OnInit, OnDestroy {
+  protected operator = from(['🥚', '🥕', '🥩']);
 
-  public readonly controllerButtons: { [key: string]: ButtonController[] } = {
-    [this.MAIN_ID]: [
-      { value: '🏠', type: ObservableEventType.ERROR, controllerId: this.MAIN_ID, enabled: false },
-      { value: '🖐️', type: ObservableEventType.COMPLETE, controllerId: this.MAIN_ID, enabled: false },
-      { value: '🍎', type: ObservableEventType.NEXT, controllerId: this.MAIN_ID, enabled: false },
-      { value: '🍌', type: ObservableEventType.NEXT, controllerId: this.MAIN_ID, enabled: false },
-      { value: '🥝', type: ObservableEventType.NEXT, controllerId: this.MAIN_ID, enabled: false },
-    ]
-  };
+  public readonly controllerButtons: { [key: string]: ButtonController[] } = {};
 
-  public readonly conveyorsWorking: { [key: string]: BehaviorSubject<boolean> } = {
-    [this.MAIN_ID]: new BehaviorSubject<boolean>(false),
-  };
+  public readonly conveyorsWorking: { [key: string]: BehaviorSubject<boolean> } = {};
 
-  public constructor(titleService: Title, metaService: Meta) {
-    super(titleService, metaService, 'from');
+  public constructor(private readonly titleService: Title, private readonly metaService: Meta) {}
+
+  public ngOnInit() {
+    this.titleService.setTitle('From rxjs');
+    this.metaService.updateTag({ name: 'description', content: 'Explicación del operador rxjs from' });
   }
 
-  protected moveElement(e: ElementInConveyor): void {
-    // TODO
-  }
-
-  protected isElementDeliveredToOperator(e: ElementInConveyor): boolean {
-    // TODO
-    return false;
-  }
-
-  protected isElementDeliveredToSubscriber(e: ElementInConveyor): boolean {
-    // TODO
-    return false;
-  }
-
-  protected addElementToBeginningOfConveyor(conveyorId: string, type: ObservableEventType, value: string) {
-    // TODO
-  }
-
-  public override elementReachesOperatorNextHook(e: ElementInConveyor) {
-    // TODO
-  }
-
-  public override elementReachesOperatorErrorHook(e: ElementInConveyor) {
-    // TODO
-  }
-
-  public override elementReachesOperatorCompleteHook(e: ElementInConveyor) {
-    // TODO
-  }
-
-  public override onOperatorConveyorDeliverElement(e: ElementInConveyor) {
-    // TODO
-  }
-
-  public override onSubscribeHook() {
-    // TODO
-  }
-
-  protected onOperatorDeliverNextEvent(value: string): void {
-   // TODO
-  }
-
-  protected onOperatorDeliverErrorEvent(value: string): void {
-    // TODO
-  }
-
-  protected onOperatorDeliverCompleteEvent(): void {
-    // TODO
+  public ngOnDestroy(): void {
+    this.metaService.removeTag('name="description"');
   }
 }
