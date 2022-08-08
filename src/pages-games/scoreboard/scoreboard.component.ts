84cb7c6187954fd14489e3cdf0e3a4d8f8a2db3f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-08 21:08:13 +0200

    Build table for players and rounds

--- src/pages-games/scoreboard/scoreboard.component.ts
+++ src/pages-games/scoreboard/scoreboard.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { StartGamePopUpOutput } from './start-game-pop-up/start-game-pop-up.contract';
 
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
   styleUrls: ['./scoreboard.component.scss'],
 })
-export class ScoreboardComponent {}
+export class ScoreboardComponent {
+  public showStartGamePopUp = true;
+
+  public players: string[] = [];
+  public rounds: string[][] = [];
+
+  public getTotalScore(player: number): number {
+    return this.rounds.reduce((score, round) => score + +round[player], 0);
+  }
+
+  public setFocus(round: number, player: number, e: Event) {
+    (e.target as HTMLElement).style.border = '2px solid blue';
+  }
+
+  public removeFocus(e: Event) {
+    (e.target as HTMLElement).style.border = '1px solid black';
+  }
+
+  public prepareTable({ players, numberOfRounds }: StartGamePopUpOutput) {
+    this.showStartGamePopUp = false;
+
+    this.players = players;
+    this.rounds = new Array(numberOfRounds).fill(new Array(this.players.length).fill(''));
+  }
+}
