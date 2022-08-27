e2f76ab93a9ff82fa41b9a71a98d6446f09e6ad4
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-27 15:01:02 +0200

    Implement UI restart game pop up

--- src/pages-games/score/restart-game-pop-up/restart-game-pop-up.component.ts
+++ src/pages-games/score/restart-game-pop-up/restart-game-pop-up.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-restart-game-pop-up',
   templateUrl: './restart-game-pop-up.component.html',
   styleUrls: ['./restart-game-pop-up.component.scss'],
 })
-export class RestartGamePopUpComponent {}
+export class RestartGamePopUpComponent {
+  @Output()
+  public confirmation = new EventEmitter<boolean>();
+}
