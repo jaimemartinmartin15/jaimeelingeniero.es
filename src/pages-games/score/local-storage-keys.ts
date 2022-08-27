6b950f082a42c28ef51aad8a87ac20ca493f91f1
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-27 16:44:49 +0200

    Logic to show or not the restart pop up game on load page

--- src/pages-games/score/local-storage-keys.ts
+++ src/pages-games/score/local-storage-keys.ts
@@ -1,1 +1,2 @@
 export const PREVIOUS_GAME_KEY = 'previousGame';
+export const PREVIOUS_GAME_DATE_KEY = 'previousGameDate';
--- src/pages-games/score/ranking/ranking.component.ts
+++ src/pages-games/score/ranking/ranking.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PREVIOUS_GAME_DATE_KEY } from '../local-storage-keys';
 
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.scss'],
 })
-export class RankingComponent {}
+export class RankingComponent implements OnInit {
+  public showRestartGamePopUp = false;
+
+  public ngOnInit(): void {
+    this.checkRestartGame();
+  }
+
+  private checkRestartGame() {
+    const lastSavedGameDate = localStorage.getItem(PREVIOUS_GAME_DATE_KEY);
+    const twoHoursAgo = Date.now() - 2 * 60 * 60 * 1000;
+    if (lastSavedGameDate != null && Date.now() - +lastSavedGameDate < twoHoursAgo) {
+      this.showRestartGamePopUp = true;
+    }
+  }
+
+  public onConfirmRestartGame(restartGame: boolean) {
+    this.showRestartGamePopUp = false;
+
+    // TODO
+  }
+}
