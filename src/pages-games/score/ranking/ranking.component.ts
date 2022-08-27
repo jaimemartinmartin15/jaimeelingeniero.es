52956fb1e86b1d6ca0369dc2483ce5697e970f4f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-27 18:51:50 +0200

    Prepare players after start game pop up is closed

--- src/pages-games/score/ranking/ranking.component.ts
+++ src/pages-games/score/ranking/ranking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PREVIOUS_GAME_DATE_KEY } from '../local-storage-keys';
 import { StartGamePopUpOutput } from '../start-game-pop-up/start-game-pop-up.contract';
+import { RankingPlayer } from './player-display/ranking-player';
 
 @Component({
   selector: 'app-ranking',
@@ -11,16 +12,25 @@ export class RankingComponent implements OnInit {
   public showRestartGamePopUp = false;
   public showStartGamePopUp = false;
 
+  public rankingPlayers: RankingPlayer[];
+
   public ngOnInit(): void {
-    this.checkRestartGame();
+    if (this.checkRestartGame()) {
+      return;
+    }
+
+    this.showStartGamePopUp = true;
   }
 
-  private checkRestartGame() {
+  private checkRestartGame(): boolean {
     const lastSavedGameDate = localStorage.getItem(PREVIOUS_GAME_DATE_KEY);
     const twoHoursAgo = Date.now() - 2 * 60 * 60 * 1000;
     if (lastSavedGameDate != null && Date.now() - +lastSavedGameDate < twoHoursAgo) {
       this.showRestartGamePopUp = true;
+      return true;
     }
+
+    return false;
   }
 
   public onConfirmRestartGame(restartGame: boolean) {
@@ -35,6 +45,7 @@ export class RankingComponent implements OnInit {
   }
 
   public prepareRanking(players: StartGamePopUpOutput) {
-    // TODO
+    this.showStartGamePopUp = false;
+    this.rankingPlayers = players.map((name, id) => ({ id, name, position: 1, scores: [], totalScore: 0 }));
   }
 }
