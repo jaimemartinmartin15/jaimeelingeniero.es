48844fe9617482e98bf029c3f0fdd274488aaecb
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-27 20:02:34 +0200

    Show enter new round popup and logic to recalculate ranking and save localstorage

--- src/pages-games/score/ranking/ranking.component.ts
+++ src/pages-games/score/ranking/ranking.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PREVIOUS_GAME_DATE_KEY } from '../local-storage-keys';
+import { PREVIOUS_GAME_DATE_KEY, PREVIOUS_GAME_KEY } from '../local-storage-keys';
+import { NextRoundPopUpInput, NextRoundPopUpOutput } from '../next-round-pop-up/next-round-pop-up.contract';
 import { StartGamePopUpOutput } from '../start-game-pop-up/start-game-pop-up.contract';
 import { RankingPlayer } from './player-display/ranking-player';
 
@@ -11,8 +12,11 @@ import { RankingPlayer } from './player-display/ranking-player';
 export class RankingComponent implements OnInit {
   public showRestartGamePopUp = false;
   public showStartGamePopUp = false;
+  public showNewRoundPopUp = false;
 
   public rankingPlayers: RankingPlayer[];
+
+  public nextRoundPopUpInput: NextRoundPopUpInput;
 
   public ngOnInit(): void {
     if (this.checkRestartGame()) {
@@ -50,6 +54,28 @@ export class RankingComponent implements OnInit {
   }
 
   public enterNewRound() {
-    // TODO
+    this.showNewRoundPopUp = true;
+    this.nextRoundPopUpInput = {
+      round: this.rankingPlayers[0].scores.length + 1,
+      players: this.rankingPlayers.map((p) => ({ ...p, punctuation: 0 })).sort((p1, p2) => p1.id - p2.id),
+    };
+  }
+
+  public onResultNewRound(output: NextRoundPopUpOutput) {
+    this.showNewRoundPopUp = false;
+    output.players.forEach((p1) => {
+      const p2 = this.rankingPlayers.find((p2) => p1.id === p2.id)!;
+      p2.scores.push(p1.punctuation);
+      p2.totalScore += p1.punctuation;
+    });
+
+    this.rankingPlayers.sort((p1, p2) => p2.totalScore - p1.totalScore);
+    const scores = this.rankingPlayers.map((p) => p.totalScore);
+    this.rankingPlayers.forEach((player) => {
+      player.position = scores.indexOf(player.totalScore) + 1;
+    });
+
+    localStorage.setItem(PREVIOUS_GAME_KEY, JSON.stringify({ players: this.rankingPlayers }));
+    localStorage.setItem(PREVIOUS_GAME_DATE_KEY, JSON.stringify(Date.now()));
   }
 }
