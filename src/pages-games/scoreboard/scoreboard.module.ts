60bcfae8e513225e8639b50ff1d6e322487fe5b8
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-08-27 13:55:12 +0200

    Created setup ranking component

--- src/pages-games/scoreboard/scoreboard.module.ts
+++ src/pages-games/scoreboard/scoreboard.module.ts
@@ -6,9 +6,10 @@ import { ScoreboardRoutingModule } from './scoreboard-routing.module';
 import { FormsModule } from '@angular/forms';
 import { StartGamePopUpComponent } from './start-game-pop-up/start-game-pop-up.component';
 import { NextRoundPopUpComponent } from './next-round-pop-up/next-round-pop-up.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 @NgModule({
   imports: [FormsModule, CommonModule, ScoreboardRoutingModule, DragDropModule],
-  declarations: [ScoreboardComponent, StartGamePopUpComponent, NextRoundPopUpComponent],
+  declarations: [ScoreboardComponent, StartGamePopUpComponent, NextRoundPopUpComponent, RankingComponent],
 })
 export class ScoreboardModule {}
