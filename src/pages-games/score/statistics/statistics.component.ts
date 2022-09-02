acc231dc5d663642a42485407926edf939feff61
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-09-02 19:18:57 +0200

    Created basic graph with lines and axis

--- src/pages-games/score/statistics/statistics.component.ts
+++ src/pages-games/score/statistics/statistics.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { intervalArray } from 'src/utils/arrays';
+import { RankingPlayer } from '../ranking/player-display/ranking-player';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
+  public colors: string[] = ['red', 'blue', 'green', 'aqua', 'silver', 'lime', 'fuchsia', 'yellow'];
 
-  constructor() { }
+  @Input()
+  public players: RankingPlayer[];
 
-  ngOnInit() {
+  public constructor(public readonly element: ElementRef) {}
+
+  public ngOnInit(): void {
+    if (this.players.length > this.colors.length) {
+      this.colors.push(
+        ...intervalArray(this.players.length - this.colors.length).map((_) => `#${Math.floor(Math.random() * 16777215).toString(16)}`)
+      );
+    }
   }
 
+  public get viewBox(): any {
+    // https://stackoverflow.com/a/33079812/14968065
+    const points = Array.from(Array(this.players.length), () => Array.from(Array(this.players[0].scores.length + 1).fill(0)));
+    this.players.forEach((player, i) => {
+      player.scores.forEach((score, j) => {
+        points[i][j + 1] = points[i][j] + score;
+      });
+    });
+
+    const minimumScore = Math.min(...points.flatMap((p) => p));
+    const maximumScore = Math.max(...points.flatMap((p) => p));
+
+    return {
+      x: 0,
+      y: minimumScore,
+      width: this.element.nativeElement.offsetWidth,
+      height: maximumScore - minimumScore,
+    };
+  }
+
+  public buildPath(player: RankingPlayer): string {
+    return player.scores.reduce((prev, _, round, scores) => {
+      return `${prev} ${(round + 1) * (this.viewBox.width / player.scores.length)},${scores
+        .slice(0, round + 1)
+        .reduce((prev, current) => prev + current, 0)}`;
+    }, 'M 2,0');
+  }
 }
