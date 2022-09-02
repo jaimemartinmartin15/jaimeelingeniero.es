7b4f18d61d36d5eee60d52de911c09d866fafcfc
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-09-02 20:48:15 +0200

    Added statistics for first and last players, and max and min round score

--- src/pages-games/score/statistics/statistics.component.ts
+++ src/pages-games/score/statistics/statistics.component.ts
@@ -50,4 +50,42 @@ export class StatisticsComponent implements OnInit {
         .reduce((prev, current) => prev + current, 0)}`;
     }, 'M 2,0');
   }
+
+  public get firstPlayers(): string {
+    const maxTotalScore = Math.max(...this.players.map((p) => p.totalScore));
+    return this.players
+      .filter((p) => p.totalScore === maxTotalScore)
+      .map((p) => p.name)
+      .join(', ');
+  }
+
+  public get lastPlayers(): string {
+    const minTotalScore = Math.min(...this.players.map((p) => p.totalScore));
+    return this.players
+      .filter((p) => p.totalScore === minTotalScore)
+      .map((p) => p.name)
+      .join(', ');
+  }
+
+  public get maxScoreInRound(): number {
+    return Math.max(...this.players.flatMap((p) => p.scores));
+  }
+
+  public get minScoreInRound(): number {
+    return Math.min(...this.players.flatMap((p) => p.scores));
+  }
+
+  public get playersMaxScoreInRound(): string {
+    return this.players
+      .filter((p) => p.scores.indexOf(this.maxScoreInRound) !== -1)
+      .map((p) => p.name)
+      .join(', ');
+  }
+
+  public get playersMinScoreInRound(): string {
+    return this.players
+      .filter((p) => p.scores.indexOf(this.minScoreInRound) !== -1)
+      .map((p) => p.name)
+      .join(', ');
+  }
 }
