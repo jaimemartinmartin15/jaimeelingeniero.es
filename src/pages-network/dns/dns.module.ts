2cc0fb8559aa29cf0cba2a2f2aa0de3800f7cf5e
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-10-06 20:37:39 +0200

    Explain NS record

--- src/pages-network/dns/dns.module.ts
+++ src/pages-network/dns/dns.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DnsComponent } from './dns.component';
 import { DnsRoutingModule } from './dns-routing.module';
 import { SoaComponent } from './rr/soa/soa.component';
+import { NsComponent } from './rr/ns/ns.component';
 
 @NgModule({
   imports: [CommonModule, DnsRoutingModule],
-  declarations: [DnsComponent, SoaComponent],
+  declarations: [DnsComponent, SoaComponent, NsComponent],
 })
 export class DnsModule {}
---
+++ src/pages-network/dns/rr/ns/ns.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ns',
+  templateUrl: './ns.component.html',
+  styles: [':host{display:block}'],
+})
+export class NsComponent {}
