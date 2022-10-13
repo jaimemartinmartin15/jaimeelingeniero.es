87c361fd426d20ff2a61a20ca3ea84b09b431e6b
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-10-13 20:18:24 +0200

    Explain A record

--- src/pages-network/dns/dns.module.ts
+++ src/pages-network/dns/dns.module.ts
@@ -4,9 +4,10 @@ import { DnsComponent } from './dns.component';
 import { DnsRoutingModule } from './dns-routing.module';
 import { SoaComponent } from './rr/soa/soa.component';
 import { NsComponent } from './rr/ns/ns.component';
+import { AComponent } from './rr/a/a.component';
 
 @NgModule({
   imports: [CommonModule, DnsRoutingModule],
-  declarations: [DnsComponent, SoaComponent, NsComponent],
+  declarations: [DnsComponent, SoaComponent, NsComponent, AComponent],
 })
 export class DnsModule {}
---
+++ src/pages-network/dns/rr/a/a.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.component.html',
+  styles: [':host{display:block}'],
+})
+export class AComponent {}
