0ac7fb96640fc788503785c171ab93c94a225c2f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-10-13 20:38:32 +0200

    Explain AAAA record

--- src/pages-network/dns/dns.module.ts
+++ src/pages-network/dns/dns.module.ts
@@ -5,9 +5,10 @@ import { DnsRoutingModule } from './dns-routing.module';
 import { SoaComponent } from './rr/soa/soa.component';
 import { NsComponent } from './rr/ns/ns.component';
 import { AComponent } from './rr/a/a.component';
+import { AAAAComponent } from './rr/aaaa/aaaa.component';
 
 @NgModule({
   imports: [CommonModule, DnsRoutingModule],
-  declarations: [DnsComponent, SoaComponent, NsComponent, AComponent],
+  declarations: [DnsComponent, SoaComponent, NsComponent, AComponent, AAAAComponent],
 })
 export class DnsModule {}
---
+++ src/pages-network/dns/rr/aaaa/aaaa.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aaaa',
+  templateUrl: './aaaa.component.html',
+  styles: [':host{display: block}'],
+})
+export class AAAAComponent {}
