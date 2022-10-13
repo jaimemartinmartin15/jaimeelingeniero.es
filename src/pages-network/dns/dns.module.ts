aab989570f93c9a646943cdb6eed02c0a0587780
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-10-13 21:02:31 +0200

    Explain MX record

--- src/pages-network/dns/dns.module.ts
+++ src/pages-network/dns/dns.module.ts
@@ -6,9 +6,10 @@ import { SoaComponent } from './rr/soa/soa.component';
 import { NsComponent } from './rr/ns/ns.component';
 import { AComponent } from './rr/a/a.component';
 import { AAAAComponent } from './rr/aaaa/aaaa.component';
+import { MxComponent } from './rr/mx/mx.component';
 
 @NgModule({
   imports: [CommonModule, DnsRoutingModule],
-  declarations: [DnsComponent, SoaComponent, NsComponent, AComponent, AAAAComponent],
+  declarations: [DnsComponent, SoaComponent, NsComponent, AComponent, AAAAComponent, MxComponent],
 })
 export class DnsModule {}
---
+++ src/pages-network/dns/rr/mx/mx.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mx',
+  templateUrl: './mx.component.html',
+  styles: [':host{display:block}'],
+})
+export class MxComponent {}
