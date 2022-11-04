e1a550658f668a8d72a24c43a6c32b9f8a2dffc9
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-11-04 19:41:03 +0100

    Wait to load style and script files for sintax highlight rxjs

---
+++ src/pages-rxjs/routable-lateral-menu/routable-lateral-menu.resolver.ts
@@ -0,0 +1,41 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoutableLateralMenuResolver implements Resolve<unknown[]> {
+  public constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  public resolve(): Observable<unknown[]> | Promise<unknown[]> | unknown[] {
+    const filesToLoad = [];
+
+    // loads style for syntax highlight
+    filesToLoad.push(this.loadStyle('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/default.min.css'));
+
+    // loads script for syntax highlight
+    filesToLoad.push(this.loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/highlight.min.js'));
+
+    return Promise.all(filesToLoad);
+  }
+
+  private loadScript(src: string) {
+    return new Promise((resolve) => {
+      const highlightScript = this.document.createElement('script');
+      highlightScript.type = 'application/javascript';
+      highlightScript.src = src;
+      this.document.head.appendChild(highlightScript);
+      highlightScript.addEventListener('load', resolve);
+    });
+  }
+
+  private loadStyle(href: string) {
+    return new Promise((resolve) => {
+      const highlightStyle = this.document.createElement('link');
+      highlightStyle.rel = 'stylesheet';
+      highlightStyle.href = href;
+      this.document.head.appendChild(highlightStyle);
+      highlightStyle.addEventListener('load', resolve);
+    });
+  }
+}
