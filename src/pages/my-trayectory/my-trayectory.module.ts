af80194ba152115cf058c5d6d36bafb792bccbc1
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-11-05 12:44:18 +0100

    Move trayectory panel component inside trayectory page

--- src/pages/my-trayectory/my-trayectory.module.ts
+++ src/pages/my-trayectory/my-trayectory.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TrayectoryPanelModule } from 'src/components/trayectory-panel/trayectory-panel.module';
-
 import { MyTrayectoryComponent } from './my-trayectory.component';
 import { MyTrayectoryRoutingModule } from './my-trayectory-routing.module';
+import { TrayectoryPanelModule } from './trayectory-panel/trayectory-panel.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MyTrayectoryRoutingModule,
-    TrayectoryPanelModule
-  ],
-  declarations: [MyTrayectoryComponent]
+  imports: [CommonModule, MyTrayectoryRoutingModule, TrayectoryPanelModule],
+  declarations: [MyTrayectoryComponent],
 })
-export class MyTrayectoryModule { }
+export class MyTrayectoryModule {}
