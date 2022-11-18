6cf5fda63bef5a324ae58622239af4d686e74212
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-11-18 19:00:18 +0100

    Created custom input number with scroll to select number of cards

--- src/pages-games/views/game-config/game-config.component.ts
+++ src/pages-games/views/game-config/game-config.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-game-config',
   templateUrl: './game-config.component.html',
   styleUrls: ['./game-config.component.scss'],
 })
-export class GameConfigComponent implements OnInit {
-  // select game name question
+export class GameConfigComponent implements OnInit, AfterViewInit {
+  // game name
   public selectGameNameDropDownOpen = false;
   public selectedGameName = 'pocha';
   public gameNames = ['pocha', 'chinchon', 'uno'];
+
+  // number of cards
+  public selectedNumberOfCards = 40;
+  public lastScrollPosition = 0;
+  @ViewChild('numberOfCards')
+  public numberOfCardsContainer: ElementRef<HTMLDivElement>;
 
   public constructor() {}
 
@@ -18,6 +24,21 @@ export class GameConfigComponent implements OnInit {
   public onSelectGameName(gameName: string) {
     this.selectedGameName = gameName;
     this.selectGameNameDropDownOpen = false;
+  }
+
+  public ngAfterViewInit(): void {
+    this.numberOfCardsContainer.nativeElement.scroll(50, 0);
+  }
+
+  public calculateNumberOfSelectedCards() {
+    const scrollContainer = this.numberOfCardsContainer.nativeElement;
+    if (scrollContainer.scrollLeft === 0) {
+      this.selectedNumberOfCards--;
+      scrollContainer.scroll(50, 0);
+    } else if (scrollContainer.scrollLeft === 100) {
+      this.selectedNumberOfCards++;
+      scrollContainer.scroll(50, 0);
+    }
   }
 
   // public onConfirmStartGame(output: StartGamePopUpOutput) {
