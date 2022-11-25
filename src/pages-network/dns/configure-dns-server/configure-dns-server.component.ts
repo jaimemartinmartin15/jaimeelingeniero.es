e027f226e8a7e8393b371bfad366e8d4940feb13
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-11-25 20:14:23 +0100

    Intro section

--- src/pages-network/dns/configure-dns-server/configure-dns-server.component.ts
+++ src/pages-network/dns/configure-dns-server/configure-dns-server.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-configure-dns-server',
   templateUrl: './configure-dns-server.component.html',
   styleUrls: ['./configure-dns-server.component.scss'],
 })
-export class ConfigureDnsServerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class ConfigureDnsServerComponent {
+  public headerPrintData = {
+    author: 'Jaime Martín Martín',
+    date: '25 de noviembre de 2022',
+  };
 }
