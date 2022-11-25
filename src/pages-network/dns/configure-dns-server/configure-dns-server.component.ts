6bc014cf33135ad8b4f078a0f7e4cc0e7a0f967f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-11-25 19:55:54 +0100

    Setup component and routing for configure dns server page

---
+++ src/pages-network/dns/configure-dns-server/configure-dns-server.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configure-dns-server',
+  templateUrl: './configure-dns-server.component.html',
+  styleUrls: ['./configure-dns-server.component.scss'],
+})
+export class ConfigureDnsServerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
