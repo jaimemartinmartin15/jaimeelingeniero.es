890941cd498d89a3f3c8a6b3bc481862200bb654
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2022-12-21 19:47:28 +0100

    Added graph legend with colors for each player

--- src/pages-games/views/statistics/progress-graph/progress-graph.component.ts
+++ src/pages-games/views/statistics/progress-graph/progress-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GameHolderService } from 'src/pages-games/game-services/game-holder.service';
 
 @Component({
@@ -6,6 +6,31 @@ import { GameHolderService } from 'src/pages-games/game-services/game-holder.ser
   templateUrl: './progress-graph.component.html',
   styleUrls: ['./progress-graph.component.scss'],
 })
-export class ProgressGraphComponent {
+export class ProgressGraphComponent implements OnInit {
+  public showPlayerGraphLines: boolean[];
+  public colors: string[] = ['#ff0000', '#0000ff', '#008000', '#804000', '#4cd3d3', '#9d9d9d', '#c32aed', '#e0e000'];
+
   public constructor(public readonly gameHolderService: GameHolderService) {}
+
+  public ngOnInit(): void {
+    this.showPlayerGraphLines = new Array(this.gameHolderService.service.players.length).fill(true);
+    this.createColorsForPlayers();
+  }
+
+  private createColorsForPlayers() {
+    const numberOfPlayersToCreateColors = this.gameHolderService.service.players.length - this.colors.length;
+    if (numberOfPlayersToCreateColors > 0) {
+      const chars = '0123456789ABCDEF';
+      for (let i = 0; i < numberOfPlayersToCreateColors; i++) {
+        let color = '#';
+        for (let j = 0; j < 6; j++) {
+          color += chars[Math.floor(Math.random() * chars.length)];
+        }
+        this.colors.push(color);
+      }
+    }
+
+    // adds transparency to all existing colors
+    this.colors = this.colors.map((c) => c + 'CC');
+  }
 }
