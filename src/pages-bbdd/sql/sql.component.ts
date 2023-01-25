f4685493617b5f75a8a3866cc143eaf9abc0a559
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-01-25 18:39:38 +0100

    Added header-print and responsive component

--- src/pages-bbdd/sql/sql.component.ts
+++ src/pages-bbdd/sql/sql.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sql',
   templateUrl: './sql.component.html',
   styleUrls: ['./sql.component.scss'],
 })
-export class SqlComponent implements OnInit {
-  public constructor() {}
+export class SqlComponent {
+  // TODO update date when finish
+  public headerPrintData = { author: 'Jaime Martín Martín', date: '25 de enero de 2023' };
 
-  public ngOnInit() {}
+  // TODO add meta tags
 }
--- src/pages-bbdd/sql/sql.module.ts
+++ src/pages-bbdd/sql/sql.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SqlComponent } from './sql.component';
 import { SqlRoutingModule } from './sql-routing.module';
+import { HeaderPrintModule } from 'src/components/header-print/header-print.module';
 
 @NgModule({
-  imports: [CommonModule, SqlRoutingModule],
+  imports: [CommonModule, SqlRoutingModule, HeaderPrintModule],
   declarations: [SqlComponent],
 })
 export class SqlModule {}
