6b838af8ed8d67d185c9a9a332bd91e5d81acd41
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-02-06 20:20:32 +0100

    Created terminal code component

--- src/pages-network/dns/dns.module.ts
+++ src/pages-network/dns/dns.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { HeaderPrintModule } from 'src/components/header-print/header-print.module';
 
-import { DnsComponent } from './dns.component';
+import { ConfigureDnsServerComponent } from './configure-dns-server/configure-dns-server.component';
 import { DnsRoutingModule } from './dns-routing.module';
-import { SoaComponent } from './rr/soa/soa.component';
-import { NsComponent } from './rr/ns/ns.component';
+import { DnsComponent } from './dns.component';
+
 import { AComponent } from './rr/a/a.component';
 import { AAAAComponent } from './rr/aaaa/aaaa.component';
-import { MxComponent } from './rr/mx/mx.component';
 import { CnameComponent } from './rr/cname/cname.component';
-import { PtrComponent } from './rr/ptr/ptr.component';
-import { WksComponent } from './rr/wks/wks.component';
 import { HinfoComponent } from './rr/hinfo/hinfo.component';
+import { MxComponent } from './rr/mx/mx.component';
+import { NsComponent } from './rr/ns/ns.component';
+import { PtrComponent } from './rr/ptr/ptr.component';
+import { SoaComponent } from './rr/soa/soa.component';
 import { TxtComponent } from './rr/txt/txt.component';
-import { ConfigureDnsServerComponent } from './configure-dns-server/configure-dns-server.component';
+import { WksComponent } from './rr/wks/wks.component';
 
 @NgModule({
   imports: [CommonModule, DnsRoutingModule, HeaderPrintModule],
---
+++ src/shared/components/terminal-code/terminal-code.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terminal-code',
+  templateUrl: './terminal-code.component.html',
+  styleUrls: ['./terminal-code.component.scss'],
+})
+export class TerminalCodeComponent {}
