89e9f87ccce7a33137614c3f8e49451ef697f256
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-02-26 13:57:54 +0100

    Implemented table with list of verbs

--- src/pages-english/tenses/list/list.component.ts
+++ src/pages-english/tenses/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Verb } from '../verb';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  public constructor() {}
+  public verbs: Verb[];
 
-  public ngOnInit() {}
+  public constructor(private readonly activatedRoute: ActivatedRoute) {}
+
+  public ngOnInit() {
+    this.verbs = this.activatedRoute.snapshot.data['listOfVerbs'];
+  }
 }
