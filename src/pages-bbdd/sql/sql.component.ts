6584361a13f5794a7f3f54ae5b192b9193faf1e1
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-04-06 12:09:21 +0200

    Review, additions and added section with examples

--- src/pages-bbdd/sql/sql.component.ts
+++ src/pages-bbdd/sql/sql.component.ts
@@ -10,26 +10,15 @@ export class SqlComponent {
   public headerPrintData = { author: 'Jaime Martín Martín', date: '25 de enero de 2023' };
   public collapsedTables = {
     createTableMechanic: false,
+    createTablePropulsion: true,
     createTableBrand: true,
     createTableConcessionaire: true,
-    createTableCar: true,
-    createTableRepair: true,
-    createTableOffer: true,
     createTableClient: true,
+    createTableCar: true,
+    createTableOffer: true,
+    createTableRepair: true,
     createTableSale: true,
   }
-  public carsTable = [
-    { id: 0, brand: 'Renault', name: 'Megane', registration: '0012TER', propulsion: 'Diésel' },
-    { id: 1, brand: 'Seat', name: 'Leon', registration: '8435POM', propulsion: 'Diésel' },
-    { id: 2, brand: 'Ford', name: 'Fiesta', registration: '1468AEM', propulsion: 'Gasolina' },
-    { id: 3, brand: 'Ford', name: 'Focus', registration: '1493UGF', propulsion: 'Diésel' },
-    { id: 4, brand: 'Ford', name: 'Transit', registration: '7934HMQ', propulsion: 'Gasolina' },
-    { id: 5, brand: 'Renault', name: 'Clio', registration: '8561ÑHB', propulsion: 'Gasolina' },
-    { id: 6, brand: 'Seat', name: 'Arona', registration: '7591MIN', propulsion: 'Diésel' },
-    { id: 7, brand: 'Citroen', name: 'Cactus', registration: '7543REB', propulsion: 'Gasolina' },
-    { id: 8, brand: 'Audi', name: 'A3', registration: '1254UNR', propulsion: 'Diésel' },
-    { id: 9, brand: 'Tesla', name: 'Model X', registration: '3495OWZ', propulsion: 'Eléctrico' },
-  ];
 
   // TODO add meta tags
 }
