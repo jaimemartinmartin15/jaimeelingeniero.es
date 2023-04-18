ad80d2db6c762921324934311324b1babdd84941
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-04-18 21:07:42 +0200

    Created SeoService and listen events to update meta tags

---
+++ src/shared/services/seo.service.ts
@@ -0,0 +1,56 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeoService {
+  public constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly metaService: Meta,
+    private readonly router: Router
+  ) {}
+
+  public listenNavigationEvents() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        switchMap(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild != null) route = route.firstChild;
+          return route.data;
+        })
+      )
+      .subscribe(({ metaTags }) => {
+        this.updateDescription(metaTags.description);
+        this.updateKeyworkds(metaTags.keywords);
+        this.setCanonical();
+      });
+
+    this.router.events.pipe(filter((event) => event instanceof NavigationStart)).subscribe(() => {
+      this.metaService.removeTag('name="description"');
+      this.metaService.removeTag('name="keywords"');
+    });
+  }
+
+  public updateDescription(description: string) {
+    this.metaService.updateTag({ name: 'description', content: description });
+  }
+
+  public updateKeyworkds(keyworkds: string[]) {
+    this.metaService.updateTag({ name: 'keywords', content: keyworkds.join(', ') });
+  }
+
+  public setCanonical() {
+    if (!location.hostname.includes('www') || location.protocol === 'http') {
+      const canonicalLink = this.document.createElement('link');
+      canonicalLink.rel = 'canonical';
+      canonicalLink.href = `https://www.jaimeelingeniero.es${location.href.substring(location.origin.length)}`;
+      this.document.head.appendChild(canonicalLink);
+    }
+  }
+}
