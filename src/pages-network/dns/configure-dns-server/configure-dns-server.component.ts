2aa60a2cfc588e865165aafc50bd2ca5fcee718c
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-04-19 20:18:13 +0200

    Refactor metaTags and routing for dns pages

--- src/pages-network/dns/configure-dns-server/configure-dns-server.component.ts
+++ src/pages-network/dns/configure-dns-server/configure-dns-server.component.ts
@@ -1,29 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-configure-dns-server',
   templateUrl: './configure-dns-server.component.html',
   styleUrls: ['./configure-dns-server.component.scss'],
 })
-export class ConfigureDnsServerComponent implements OnInit, OnDestroy {
+export class ConfigureDnsServerComponent {
   public headerPrintData = {
     author: 'Jaime Martín Martín',
     date: '3 de diciembre de 2022',
   };
-
-  public constructor(private readonly metaService: Meta) {}
-
-  public ngOnInit(): void {
-    this.metaService.updateTag({
-      name: 'description',
-      content: 'Configuración desde cero de un servidor DNS BIND9.',
-    });
-    this.metaService.updateTag({ name: 'keywords', content: 'dns, servidor, configuración' });
-  }
-
-  public ngOnDestroy(): void {
-    this.metaService.removeTag('name="description"');
-    this.metaService.removeTag('name="keywords"');
-  }
 }
