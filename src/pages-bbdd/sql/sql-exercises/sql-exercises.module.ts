afa92018f597e5b0323af416611abd94ca9aad4d
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-04-27 19:50:17 +0200

    Styles and intro

--- src/pages-bbdd/sql/sql-exercises/sql-exercises.module.ts
+++ src/pages-bbdd/sql/sql-exercises/sql-exercises.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SqlExercisesRoutingModule } from './sql-exercises-routing.module';
 import { SqlExercisesComponent } from './sql-exercises.component';
+import { HeaderPrintModule } from 'src/components/header-print/header-print.module';
 
 @NgModule({
-  imports: [CommonModule, SqlExercisesRoutingModule],
+  imports: [CommonModule, SqlExercisesRoutingModule, HeaderPrintModule],
   declarations: [SqlExercisesComponent],
 })
 export class SqlExercisesModule {}
