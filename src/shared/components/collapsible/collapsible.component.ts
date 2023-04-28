0ee7181c832404e0350fc36becd5db50c52735d8
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-04-28 20:45:02 +0200

    Created collapsible component

---
+++ src/shared/components/collapsible/collapsible.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, Input, AfterViewInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-collapsible',
+  templateUrl: './collapsible.component.html',
+  styleUrls: ['./collapsible.component.scss'],
+})
+export class CollapsibleComponent implements AfterViewInit {
+  @ViewChild('collapsibleContent', { static: false })
+  private collapsibleContent: ElementRef;
+
+  @Input()
+  public isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+    this.isOpen ? this.openCollapsible() : this.closeCollapsible();
+  }
+
+  public ngAfterViewInit(): void {
+    this.isOpen ? this.openCollapsible() : this.closeCollapsible();
+  }
+
+  private openCollapsible() {
+    this.collapsibleContent.nativeElement.style.height = this.collapsibleContent.nativeElement.scrollHeight + 'px';
+  }
+
+  private closeCollapsible() {
+    this.collapsibleContent.nativeElement.style.height = '0px';
+  }
+}
