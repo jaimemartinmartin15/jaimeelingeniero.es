58c37026db64e7426b682ed005a86d65ba1052bf
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-06-14 19:39:36 +0200

    Distinguish between edition or not in game-config

--- src/pages-games/components/round-info/round-info.component.ts
+++ src/pages-games/components/round-info/round-info.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GameHolderService } from 'src/pages-games/game-services/game-holder.service';
+import { ROUTING_PATHS } from 'src/pages-games/routing-paths';
 
 @Component({
   selector: 'app-round-info',
@@ -7,5 +9,14 @@ import { GameHolderService } from 'src/pages-games/game-services/game-holder.ser
   styleUrls: ['./round-info.component.scss'],
 })
 export class RoundInfoComponent {
-  public constructor(public readonly gameHolderService: GameHolderService) {}
+  public constructor(
+    public readonly gameHolderService: GameHolderService,
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
+
+  @HostListener('click')
+  private navigateToChangeConfig() {
+    this.router.navigate(['../', ROUTING_PATHS.CHANGE_CONFIG], { relativeTo: this.activatedRoute });
+  }
 }
