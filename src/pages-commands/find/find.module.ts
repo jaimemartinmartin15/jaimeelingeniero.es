2da79472ede302274e372bc99a22ffab1dc2b12d
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-04 14:58:23 +0200

    Refactor commands page, and adapt find page structure

--- src/pages-commands/find/find.module.ts
+++ src/pages-commands/find/find.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HeaderPrintModule } from 'src/components/header-print/header-print.module';
 import { TerminalCodeModule } from 'src/shared/components/terminal-code/terminal-code.module';
 import { FindRoutingModule } from './find-routing.module';
 import { FindComponent } from './find.component';
 
 @NgModule({
-  imports: [CommonModule, FindRoutingModule, HeaderPrintModule, TerminalCodeModule],
+  imports: [CommonModule, FindRoutingModule, TerminalCodeModule],
   declarations: [FindComponent],
 })
 export class FindModule {}
