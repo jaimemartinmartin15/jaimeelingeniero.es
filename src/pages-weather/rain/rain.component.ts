f5eb1421c826ecc53e3fa0083aadd5f0a107bb01
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-04 20:07:23 +0200

    Fetch rain data and configure asset

--- src/pages-weather/rain/rain.component.ts
+++ src/pages-weather/rain/rain.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,9 +15,14 @@ export class RainComponent implements OnInit {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   ];
 
-  constructor() {}
+  public constructor(private readonly http: HttpClient) {}
 
-  ngOnInit() {}
+  public ngOnInit() {
+    this.http.get('assets/pages-weather/rain/rain-data.txt', { responseType: 'text' }).subscribe((response) => {
+      // TODO parse data
+      console.log(response);
+    });
+  }
 
   public get weekDayIndexMonthStarts(): number {
     return new Date(this.currentYear, this.currentMonthIndex, 1).getDay();
--- src/pages-weather/rain/rain.module.ts
+++ src/pages-weather/rain/rain.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RainComponent } from './rain.component';
 import { RainRoutingModule } from './rain-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, RainRoutingModule],
+  imports: [CommonModule, RainRoutingModule, HttpClientModule],
   declarations: [RainComponent],
 })
 export class RainModule {}
