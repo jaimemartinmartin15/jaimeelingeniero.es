d63b7d9a53db5c82457a92fe575bfaf5d58f412b
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-04 19:51:24 +0200

    Calendar layout

--- src/pages-weather/rain/rain.component.ts
+++ src/pages-weather/rain/rain.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rain.component.scss'],
 })
 export class RainComponent implements OnInit {
+  public currentYear = new Date().getFullYear();
+  public currentMonthIndex = new Date().getMonth();
+
+  // TODO remove this mock
+  public monthDaysLiters: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+
   constructor() {}
 
   ngOnInit() {}
+
+  public get weekDayIndexMonthStarts(): number {
+    return new Date(this.currentYear, this.currentMonthIndex, 1).getDay();
+  }
 }
