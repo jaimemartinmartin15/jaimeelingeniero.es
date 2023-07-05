936c773a8815bb6a472515e748057f956e1509e7
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-05 18:22:21 +0200

    Improved pluviometer icon

--- src/pages-weather/rain/rain.component.ts
+++ src/pages-weather/rain/rain.component.ts
@@ -11,8 +11,8 @@ export class RainComponent implements OnInit {
   public readonly MONTHS = MONTHS;
   public currentYear = new Date().getFullYear();
   public currentMonthIndex = new Date().getMonth();
-  public monthDays: { percentage: number; liters: number }[] = [];
-  public historical: { date: Date; percentage: number; liters: number }[] = [];
+  public monthDays: { svgOffset: number; liters: number }[] = [];
+  public historical: { date: Date; svgOffset: number; liters: number }[] = [];
 
   public constructor(private readonly http: HttpClient) {}
 
@@ -20,8 +20,11 @@ export class RainComponent implements OnInit {
     this.http.get('assets/pages-weather/rain/rain-data.txt', { responseType: 'text' }).subscribe((response) => {
       this.historical = response.split('\n').map((line) => {
         const date = line.split(/\/|-|\r/); // \r is the new line, so the liters do not contain it
-        const percentage = (+date[3] * 0.8) / 35; // pluviometer makes 35 liters but it only fills 80% in the animation
-        return { date: new Date(+date[2], +date[1] - 1, +date[0]), percentage, liters: +date[3] };
+        // svgOffset: 360 is svg viewBox height (363 avoids shadow). Pluviometer: offset 0 -> full water (35 L)    offset 360 -> empty water (0 L)
+        // start from offset 185 to not fill full pluviometer  (difference 178)   offset 185 -> 35 L     offset 363 -> 0 L
+        // if it rains more than 70 do not take it into account (negative offset)
+        const svgOffset = 363 - 178 * (Math.min(+date[3], 70) / 35);
+        return { date: new Date(+date[2], +date[1] - 1, +date[0]), svgOffset, liters: +date[3] };
       });
 
       this.setMonthDays(this.currentYear, this.currentMonthIndex);
