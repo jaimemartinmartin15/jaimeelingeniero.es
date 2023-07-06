3f38940f7facfe13dbb381bf1f27ffc70d8739f3
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-06 17:33:54 +0200

    Use # instead of // to add comments

--- src/pages-weather/rain/rain-data.resolver.ts
+++ src/pages-weather/rain/rain-data.resolver.ts
@@ -11,10 +11,10 @@ export class RainDataResolver implements Resolve<string[]> {
     return this.http.get('assets/pages-weather/rain/rain-data.txt', { responseType: 'text' }).pipe(
       map(
         // ignore commented and empty lines
-        (response) => response.split('\n').filter((l) => !l.trim().startsWith('//') && l.trim() !== '')
+        (response) => response.split('\n').filter((l) => !l.trim().startsWith('#') && l.trim() !== '')
       ),
       // remove comments in lines that contain data
-      map((lines) => lines.map((l) => l.substring(0, l.indexOf('//') !== -1 ? l.indexOf('//') : l.length).trim()))
+      map((lines) => lines.map((l) => l.substring(0, l.indexOf('#') !== -1 ? l.indexOf('#') : l.length).trim()))
     );
   }
 }
