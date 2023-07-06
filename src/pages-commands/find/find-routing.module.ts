ebd3fb82a24a33f80a4faf7935e8a4bdebc8ccf2
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-06 20:03:16 +0200

    Added missing url to sitemap for find command and metaTags

--- src/pages-commands/find/find-routing.module.ts
+++ src/pages-commands/find/find-routing.module.ts
@@ -6,7 +6,12 @@ const routes: Routes = [
   {
     path: '',
     component: FindComponent,
+    title: 'Find',
     data: {
+      metaTags: {
+        description: 'Aprende a usar el comando find y saca todo su potencial.',
+        keywords: ['bash', 'comandos', 'find'],
+      },
       headerPrint: {
         author: 'Jaime Martín Martín',
         date: '27 de junio de 2023',
