c2097437e1f9555cc9de85ef8ec0526d660e2d91
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-11 18:51:16 +0200

    Using scroll-snap and refactor html. Implement some logic on scrolling

--- src/pages-weather/rain/rain.component.ts
+++ src/pages-weather/rain/rain.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MONTHS } from 'src/utils/dates';
 import { RainData } from './rain-data';
@@ -10,15 +10,24 @@ import { RainDataService } from './rain-data.service';
   styleUrls: ['./rain.component.scss'],
   providers: [RainDataService],
 })
-export class RainComponent implements OnInit {
+export class RainComponent implements OnInit, AfterViewInit {
+  @ViewChild('monthGraphicScroller', { static: false }) monthGraphicScrollerRef: ElementRef;
+  @ViewChild('yearGraphicScroller', { static: false }) yearGraphicScrollerRef: ElementRef;
+
   public readonly MONTHS = MONTHS;
   public readonly popUp = { show: false, content: '' };
 
   public selectedYear = new Date().getFullYear();
   public selectedMonth = new Date().getMonth();
 
+  // TODO populate previous and next arrays
+  public previousDaysRainData: RainData[] = [];
   public daysRainData: RainData[] = [];
+  public nextDaysRainData: RainData[] = [];
+
+  public previousMonthsRainData: RainData[] = [];
   public monthsRainData: RainData[] = [];
+  public nextMonthsRainData: RainData[] = [];
 
   public constructor(public readonly rainDataService: RainDataService, private readonly activatedRoute: ActivatedRoute) {}
 
@@ -26,6 +35,12 @@ export class RainComponent implements OnInit {
     this.rainDataService.setData(this.activatedRoute.snapshot.data['rainData']);
     this.daysRainData = this.rainDataService.getRainDataForDaysInMonthAndYear(this.selectedMonth, this.selectedYear);
     this.monthsRainData = this.rainDataService.getRainDataForMonthsInYear(this.selectedYear);
+  }
+
+  public ngAfterViewInit(): void {
+    // starts the scrolling in the middle element
+    this.monthGraphicScrollerRef.nativeElement.scrollLeft = this.monthGraphicScrollerRef.nativeElement.clientWidth;
+    this.yearGraphicScrollerRef.nativeElement.scrollLeft = this.yearGraphicScrollerRef.nativeElement.clientWidth;
   }
 
   public get showBadgeForSelectedMonthAndYear(): boolean {
@@ -118,6 +133,44 @@ export class RainComponent implements OnInit {
     if (day.popUpContent !== undefined) {
       this.popUp.show = true;
       this.popUp.content = day.popUpContent;
+    }
+  }
+
+  public onScrollingMonth(e: Event) {
+    const scroller = e.target as HTMLDivElement;
+    const childrenWidth = scroller.children[0].clientWidth;
+    const gapSize = 30; // 30 px as in scss file in .graphic-scroller class
+
+    // check if the scroll is multiple of a child width taking into account the gap
+    if (scroller.scrollLeft % (childrenWidth + gapSize) === 0) {
+      if (scroller.scrollLeft === 0) {
+        // scroll to previous month
+        scroller.scrollLeft = childrenWidth + gapSize;
+        this.showPreviousMonth();
+      } else if (scroller.scrollLeft === (childrenWidth + gapSize) * 2) {
+        // scroll to next month
+        scroller.scrollLeft = childrenWidth + gapSize;
+        this.showNextMonth();
+      }
+    }
+  }
+
+  public onScrollinYear(e: Event) {
+    const scroller = e.target as HTMLDivElement;
+    const childrenWidth = scroller.children[0].clientWidth;
+    const gapSize = 30; // 30 px as in scss file in .graphic-scroller class
+
+    // check if the scroll is multiple of a child width taking into account the gap
+    if (scroller.scrollLeft % (childrenWidth + gapSize) === 0) {
+      if (scroller.scrollLeft === 0) {
+        // scroll to previous year
+        scroller.scrollLeft = childrenWidth + gapSize;
+        this.showPreviousYear();
+      } else if (scroller.scrollLeft === (childrenWidth + gapSize) * 2) {
+        // scroll to next year
+        scroller.scrollLeft = childrenWidth + gapSize;
+        this.showNextYear();
+      }
     }
   }
 }
