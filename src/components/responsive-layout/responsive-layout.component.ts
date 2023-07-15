6a5e3e5ea7f94e5504d1bfb434f34c76fca42545
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-15 13:56:52 +0200

    Responsive layout for tablet and mobile

--- src/components/responsive-layout/responsive-layout.component.ts
+++ src/components/responsive-layout/responsive-layout.component.ts
@@ -1,11 +1,11 @@
-import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, OnInit, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-responsive-layout',
   templateUrl: './responsive-layout.component.html',
   styleUrls: ['./responsive-layout.component.scss'],
 })
-export class ResponsiveLayoutComponent implements AfterContentInit {
+export class ResponsiveLayoutComponent implements OnInit, AfterContentInit {
   @ContentChildren('responsiveLayoutTopHeader')
   private topHeaderContent: QueryList<HTMLElement>;
   public showTopHeader: boolean = false;
@@ -41,11 +41,22 @@ export class ResponsiveLayoutComponent implements AfterContentInit {
   ngAfterContentInit() {
     this.showTopHeader = this.topHeaderContent.length > 0;
     this.showFlexContentHeader = this.flexContentHeaderContent.length > 0;
-    this.showLeftMenu = this.leftMenuContent.length > 0;
+    this.showLeftMenu = this.leftMenuContent.length > 0 && !window.matchMedia('(max-width: 900px)').matches;
     this.showContentHeader = this.contentHeaderContent.length > 0;
     this.showContentFooter = this.contentFooterContent.length > 0;
-    this.showRightMenu = this.rightMenuContent.length > 0;
+    this.showRightMenu = this.rightMenuContent.length > 0 && !window.matchMedia('(max-width: 1150px)').matches;
     this.showFlexContentFooter = this.flexContentFooterContent.length > 0;
     this.showBottomFooter = this.bottomFooterContent.length > 0;
+  }
+
+  ngOnInit(): void {
+    // 900 = 250 (left menu) + xxx (content)
+    window.matchMedia('(max-width: 900px)').addEventListener('change', (e) => {
+      this.showLeftMenu = !e.matches && this.leftMenuContent.length > 0;
+    });
+    // 1150 = 250 (left menu) + xxx (content) + 250 (right menu)
+    window.matchMedia('(max-width: 1150px)').addEventListener('change', (e) => {
+      this.showRightMenu = !e.matches && this.rightMenuContent.length > 0;
+    });
   }
 }
