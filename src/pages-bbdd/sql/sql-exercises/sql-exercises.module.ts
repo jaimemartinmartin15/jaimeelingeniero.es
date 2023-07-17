196304d36993439df4fd2a998b548d6873421247
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-17 17:48:00 +0200

    Using responsive layout in sql exercises

--- src/pages-bbdd/sql/sql-exercises/sql-exercises.module.ts
+++ src/pages-bbdd/sql/sql-exercises/sql-exercises.module.ts
@@ -5,9 +5,10 @@ import { CollapsibleModule } from 'src/shared/components/collapsible/collapsible
 import { TerminalCodeModule } from 'src/shared/components/terminal-code/terminal-code.module';
 import { SqlExercisesRoutingModule } from './sql-exercises-routing.module';
 import { SqlExercisesComponent } from './sql-exercises.component';
+import { ResponsiveLayoutModule } from 'src/components/responsive-layout/responsive-layout.module';
 
 @NgModule({
-  imports: [CommonModule, SqlExercisesRoutingModule, HeaderPrintModule, TerminalCodeModule, CollapsibleModule],
+  imports: [CommonModule, SqlExercisesRoutingModule, HeaderPrintModule, TerminalCodeModule, CollapsibleModule, ResponsiveLayoutModule],
   declarations: [SqlExercisesComponent],
 })
 export class SqlExercisesModule {}
