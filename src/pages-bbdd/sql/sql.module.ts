4c9027eee19ac951f360e4ff36a67604728c951f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-07-19 17:01:57 +0200

    Using app-collapsible in sql page for tables

--- src/pages-bbdd/sql/sql.module.ts
+++ src/pages-bbdd/sql/sql.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SqlComponent } from './sql.component';
-import { SqlRoutingModule } from './sql-routing.module';
+import { NgModule } from '@angular/core';
+import { CollapsibleModule } from 'src/components/collapsible/collapsible.module';
 import { HeaderPrintModule } from 'src/components/header-print/header-print.module';
-import { TerminalCodeModule } from 'src/components/terminal-code/terminal-code.module';
 import { ResponsiveLayoutModule } from 'src/components/responsive-layout/responsive-layout.module';
+import { TerminalCodeModule } from 'src/components/terminal-code/terminal-code.module';
+import { SqlRoutingModule } from './sql-routing.module';
+import { SqlComponent } from './sql.component';
 
 @NgModule({
-  imports: [CommonModule, SqlRoutingModule, HeaderPrintModule, TerminalCodeModule, ResponsiveLayoutModule],
+  imports: [CommonModule, SqlRoutingModule, HeaderPrintModule, TerminalCodeModule, ResponsiveLayoutModule, CollapsibleModule],
   declarations: [SqlComponent],
 })
 export class SqlModule {}
