b7426d46525c84dcd637c89f0f88b83ff7df6f72
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-09-05 18:09:17 +0200

    Created months-graphic component

--- src/pages-weather/rain/rain.module.ts
+++ src/pages-weather/rain/rain.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { CollapsibleModule } from 'src/shared/components/collapsible/collapsible.module';
 import { DataFileSelectorComponent } from './components/data-file-selector/data-file-selector.component';
 import { DaysGraphicComponent } from './components/days-graphic/days-graphic.component';
+import { MonthsGraphicComponent } from './components/months-graphic/months-graphic.component';
 import { RainDataResolver } from './rain-data.resolver';
 import { RainRoutingModule } from './rain-routing.module';
 import { RainComponent } from './rain.component';
 
 @NgModule({
   imports: [CommonModule, RainRoutingModule, HttpClientModule, CollapsibleModule],
-  declarations: [RainComponent, DataFileSelectorComponent, DaysGraphicComponent],
+  declarations: [RainComponent, DataFileSelectorComponent, DaysGraphicComponent, MonthsGraphicComponent],
   providers: [RainDataResolver],
 })
 export class RainModule {}
