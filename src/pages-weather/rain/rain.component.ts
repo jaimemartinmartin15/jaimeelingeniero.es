334e38cd75e53fa47830099542cd66e6c506f36a
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-09-05 19:20:18 +0200

    Adapt rain component with graphic subcomponents

--- src/pages-weather/rain/rain.component.ts
+++ src/pages-weather/rain/rain.component.ts
@@ -1,7 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MONTHS } from 'src/utils/dates';
-import { RainData } from './rain-data';
+import { FileLine } from './file-line';
 import { RainDataService } from './rain-data.service';
 import { SnapScrollHelper } from './snap-scroll-helper';
 
@@ -12,95 +10,35 @@ import { SnapScrollHelper } from './snap-scroll-helper';
   providers: [RainDataService],
 })
 export class RainComponent extends SnapScrollHelper implements OnInit, AfterViewInit {
-  public readonly MONTHS = MONTHS;
   public readonly popUp = { show: false, content: '' };
+
+  public isDataFileLoading: boolean = false;
 
   public selectedYear = new Date().getFullYear();
   public selectedMonth = new Date().getMonth();
 
-  public daysGraphicPreviousRainData: RainData[] = [];
-  public daysGraphicRainData: RainData[] = [];
-  public daysGraphicNextRainData: RainData[] = [];
-
-  public monthsGraphicPreviousRainData: RainData[] = [];
-  public monthsGraphicRainData: RainData[] = [];
-  public monthsGraphicNextRainData: RainData[] = [];
-
-  public yearsGraphicRainData: RainData[] = [];
-  public yearsGraphicSvgWidth: number = 0;
-
-  public constructor(public readonly rainDataService: RainDataService, private readonly activatedRoute: ActivatedRoute) {
+  public constructor(public readonly rainDataService: RainDataService) {
     super();
   }
 
   public ngOnInit() {
-    this.rainDataService.setData(this.activatedRoute.snapshot.data['rainData']);
-    this.updateArraysRainData();
-
     this.handleScrollSnapEvents();
     this.handleDaysGraphicScroll();
     this.handleMonthsGraphicScroll();
   }
 
-  public ngAfterViewInit(): void {
+  public ngAfterViewInit() {
     this.centerScrollingInstantly();
+  }
+
+  public onLoadDataFile(fileLines: FileLine[]) {
+    this.rainDataService.setData(fileLines);
+
+    // select current month and year
+    this.selectedYear = new Date().getFullYear();
+    this.selectedMonth = new Date().getMonth();
 
     this.updateHeightsOfGraphicWrappers();
-
-    // min width of years-graphic (timeout to avoid error ExpressionChangedAfterItHasBeenCheckedError)
-    setTimeout(() => {
-      const boxWidth = this.yearsGraphicSvgWrapperRef.nativeElement.offsetWidth;
-      const boxHeight = this.yearsGraphicSvgWrapperRef.nativeElement.offsetHeight;
-      const relation = boxHeight / 325; //325 is viewBox in html file
-      this.yearsGraphicSvgWidth =
-        25 + this.yearsGraphicRainData.length * 30 < boxWidth / relation ? boxWidth / relation : 25 + this.yearsGraphicRainData.length * 30;
-    }, 0);
-  }
-
-  private updateArraysRainData() {
-    this.daysGraphicPreviousRainData = this.rainDataService.getRainDataPerDays(
-      this.selectedMonth > 0 ? this.selectedMonth - 1 : 11,
-      this.selectedMonth > 0 ? this.selectedYear : this.selectedYear - 1
-    );
-    this.daysGraphicRainData = this.rainDataService.getRainDataPerDays(this.selectedMonth, this.selectedYear);
-    this.daysGraphicNextRainData = this.rainDataService.getRainDataPerDays(
-      this.selectedMonth < 11 ? this.selectedMonth + 1 : 0,
-      this.selectedMonth < 11 ? this.selectedYear : this.selectedYear + 1
-    );
-
-    this.monthsGraphicPreviousRainData = this.rainDataService.getRainDataPerMonths(this.selectedYear - 1);
-    this.monthsGraphicRainData = this.rainDataService.getRainDataPerMonths(this.selectedYear);
-    this.monthsGraphicNextRainData = this.rainDataService.getRainDataPerMonths(this.selectedYear + 1);
-
-    this.yearsGraphicRainData = this.rainDataService.getRainDataPerYear();
-  }
-
-  public showBadgeForMonth(month: number, year: number): boolean {
-    return this.rainDataService.getRainDataPerMonths(year).find((m) => m.month === month)?.hasMessage ?? false;
-  }
-
-  public getBadgeColorForMonth(month: number, year: number): string {
-    return this.rainDataService.getRainDataPerMonths(year).find((m) => m.month === month)!.bulletColor;
-  }
-
-  public isTotalAmountOfLitersAvailableForMonth(month: number, year: number): boolean {
-    return (
-      this.rainDataService
-        .getRainDataPerMonths(year)
-        .filter((m) => !m.hasLiters)
-        .find((m) => m.month === month) != undefined
-    );
-  }
-
-  public totalAmountOfLitersInMonth(month: number, year: number): number {
-    return this.rainDataService.getRainDataPerMonths(year).find((m) => m.month === month)!.liters;
-  }
-
-  public getWeatherIcon(month: number): string {
-    if (month < 3) return '❄️';
-    if (month < 6) return '🌹';
-    if (month < 9) return '☀️';
-    return '🍁';
   }
 
   public showPreviousMonth() {
@@ -110,7 +48,6 @@ export class RainComponent extends SnapScrollHelper implements OnInit, AfterView
       this.selectedYear--;
     }
 
-    this.updateArraysRainData();
     this.updateHeightsOfGraphicWrappers();
   }
 
@@ -121,74 +58,26 @@ export class RainComponent extends SnapScrollHelper implements OnInit, AfterView
       this.selectedYear++;
     }
 
-    this.updateArraysRainData();
     this.updateHeightsOfGraphicWrappers();
-  }
-
-  public getFirstDayOfMonthWeekDayIndex(month: number, year: number): number {
-    return new Date(year, month, 1).getDay();
-  }
-
-  public isTotalAmountOfLitersAvailableForYear(year: number): boolean {
-    return this.rainDataService.getRainDataPerYear().find((y) => y.year === year)?.hasLiters ?? false;
-  }
-
-  public getTotalAmountOfLitersInYear(year: number): number {
-    return this.rainDataService.getRainDataPerYear().find((y) => y.year === year)!.liters;
   }
 
   public showPreviousYear() {
     this.selectedYear--;
-    this.updateArraysRainData();
     this.updateHeightsOfGraphicWrappers();
   }
 
   public showNextYear() {
     this.selectedYear++;
-    this.updateArraysRainData();
     this.updateHeightsOfGraphicWrappers();
   }
 
   public selectMonthOfSelectedYear(month: number) {
     this.selectedMonth = month;
-    this.updateArraysRainData();
     this.updateHeightsOfGraphicWrappers();
-  }
-
-  public showPopUpForMonth(month: number, year: number) {
-    const foundMonth = this.rainDataService.getRainDataPerMonths(year).find((m) => m.month === month);
-    if (foundMonth?.hasMessage) {
-      this.popUp.show = true;
-      this.popUp.content = foundMonth.popUpContent;
-    }
-  }
-
-  public showPopUpForDay(day: RainData) {
-    if (day.hasMessage) {
-      this.popUp.show = true;
-      this.popUp.content = day.popUpContent;
-    }
   }
 
   public selectYear(year: number) {
     this.selectedYear = year;
-    this.updateArraysRainData();
     this.updateHeightsOfGraphicWrappers();
-  }
-
-  public showBadgeForYear(year: number): boolean {
-    return this.rainDataService.getRainDataPerYear().find((y) => y.year === year)?.hasMessage ?? false;
-  }
-
-  public getBadgeColorForYear(year: number): string {
-    return this.rainDataService.getRainDataPerYear().find((y) => y.year === year)!.bulletColor;
-  }
-
-  public showPopUpForYear(year: number): void {
-    const foundYear = this.rainDataService.getRainDataPerYear().find((y) => y.year === year);
-    if (foundYear?.hasMessage) {
-      this.popUp.show = true;
-      this.popUp.content = foundYear.popUpContent;
-    }
   }
 }
