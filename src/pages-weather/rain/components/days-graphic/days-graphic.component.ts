518e7bde2f531680464b002526dce221420b643c
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-09-05 17:25:30 +0200

    Created days-graphic component

---
+++ src/pages-weather/rain/components/days-graphic/days-graphic.component.ts
@@ -0,0 +1,92 @@
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { MONTHS } from 'src/utils/dates';
+import { RainData } from '../../rain-data';
+import { RainDataService } from '../../rain-data.service';
+
+@Component({
+  selector: 'app-days-graphic',
+  templateUrl: './days-graphic.component.html',
+  styleUrls: ['./days-graphic.component.scss'],
+})
+export class DaysGraphicComponent implements OnChanges {
+  public readonly MONTHS = MONTHS;
+
+  @Input()
+  public month: number;
+
+  @Input()
+  public year: number;
+
+  @Input()
+  public loading: boolean;
+
+  @Output()
+  public showPopUp: EventEmitter<string> = new EventEmitter();
+
+  @Output()
+  public showPreviousMonth: EventEmitter<void> = new EventEmitter();
+
+  @Output()
+  public showNextMonth: EventEmitter<void> = new EventEmitter();
+
+  public rainPerDays: RainData[] = [];
+
+  public constructor(private readonly rainDataService: RainDataService) {}
+
+  public ngOnChanges(): void {
+    this.rainPerDays = this.rainDataService.getRainDataPerDays(this.month, this.year);
+  }
+
+  public existsMessageForMonth(): boolean {
+    const months = this.rainDataService.getRainDataPerMonths(this.year);
+    const foundMonth = months.find((m) => m.month === this.month);
+    return foundMonth != null && foundMonth.hasMessage;
+  }
+
+  public getBadgeColorForMonth(): string {
+    const months = this.rainDataService.getRainDataPerMonths(this.year);
+    const foundMonth = months.find((m) => m.month === this.month);
+    if (foundMonth != null && foundMonth.hasMessage) {
+      return foundMonth.bulletColor;
+    }
+
+    return 'transparent';
+  }
+
+  public showMonthPopUp() {
+    const months = this.rainDataService.getRainDataPerMonths(this.year);
+    const foundMonth = months.find((m) => m.month === this.month);
+    if (foundMonth != null && foundMonth.hasMessage) {
+      this.showPopUp.emit(foundMonth.popUpContent);
+    }
+  }
+
+  public isTotalAmountOfLitersAvailableForMonth() {
+    const months = this.rainDataService.getRainDataPerMonths(this.year);
+    const foundMonth = months.find((m) => m.month === this.month);
+    return !this.loading && foundMonth !== undefined && foundMonth.hasLiters;
+  }
+
+  public totalAmountOfLitersInMonth(): number {
+    const months = this.rainDataService.getRainDataPerMonths(this.year);
+    const foundMonth = months.find((m) => m.month === this.month);
+    return foundMonth!.liters;
+  }
+
+  public getWeatherIcon(): string {
+    if (this.month < 3) return '❄️';
+    if (this.month < 6) return '🌹';
+    if (this.month < 9) return '☀️';
+    return '🍁';
+  }
+
+  public showDayPopUp(day: RainData) {
+    if (day.hasMessage) {
+      this.showPopUp.emit(day.popUpContent);
+    }
+  }
+
+  public getFirstDayOfMonthWeekDayIndex(): number {
+    return new Date(this.year, this.month, 1).getDay();
+  }
+}
--- src/pages-weather/rain/rain.module.ts
+++ src/pages-weather/rain/rain.module.ts
@@ -3,13 +3,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CollapsibleModule } from 'src/shared/components/collapsible/collapsible.module';
 import { DataFileSelectorComponent } from './components/data-file-selector/data-file-selector.component';
+import { DaysGraphicComponent } from './components/days-graphic/days-graphic.component';
 import { RainDataResolver } from './rain-data.resolver';
 import { RainRoutingModule } from './rain-routing.module';
 import { RainComponent } from './rain.component';
 
 @NgModule({
   imports: [CommonModule, RainRoutingModule, HttpClientModule, CollapsibleModule],
-  declarations: [RainComponent, DataFileSelectorComponent],
+  declarations: [RainComponent, DataFileSelectorComponent, DaysGraphicComponent],
   providers: [RainDataResolver],
 })
 export class RainModule {}
