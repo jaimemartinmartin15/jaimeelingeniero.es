0fba63cb3904badad1919c37cc889b2524078eae
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-04 15:05:39 +0100

    Added assets and image for cd command

--- src/pages-commands/cd/cd.module.ts
+++ src/pages-commands/cd/cd.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { TerminalCodeModule } from 'src/shared/components/terminal-code/terminal-code.module';
 import { CdRoutingModule } from './cd-routing.module';
 import { CdComponent } from './cd.component';
+import { ImageFullScreenModule } from 'src/shared/directives/image-full-screen/image-full-screen.module';
 
 @NgModule({
-  imports: [CommonModule, CdRoutingModule, TerminalCodeModule],
+  imports: [CommonModule, CdRoutingModule, TerminalCodeModule, ImageFullScreenModule],
   declarations: [CdComponent],
 })
 export class CdModule {}
