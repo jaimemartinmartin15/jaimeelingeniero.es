aec033723e1fbbd067320db6858ee0cd53d4615e
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-22 16:44:36 +0100

    Created component bullets

--- src/pages-presentation/my-trayectory/my-trayectory.module.ts
+++ src/pages-presentation/my-trayectory/my-trayectory.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { MyTrayectoryRoutingModule } from './my-trayectory-routing.module';
 import { MyTrayectoryComponent } from './my-trayectory.component';
 import { TechnologiesSvgModule } from 'src/svg/generated/technologies-svg.module';
+import { BulletsComponent } from './bullets/bullets.component';
 
 @NgModule({
   imports: [CommonModule, MyTrayectoryRoutingModule, TechnologiesSvgModule],
-  declarations: [MyTrayectoryComponent],
+  declarations: [MyTrayectoryComponent, BulletsComponent],
 })
 export class MyTrayectoryModule {}
