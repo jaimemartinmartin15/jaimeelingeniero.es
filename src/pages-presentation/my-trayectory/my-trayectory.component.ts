c2d8724daf114d4a0af57a622381df4360536938
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-22 20:17:10 +0100

    Added burger for navigation bar in mobile

--- src/pages-presentation/my-trayectory/my-trayectory.component.ts
+++ src/pages-presentation/my-trayectory/my-trayectory.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-my-trayectory',
   templateUrl: './my-trayectory.component.html',
   styleUrls: ['./my-trayectory.component.scss'],
 })
-export class MyTrayectoryComponent {}
+export class MyTrayectoryComponent {
+  @ViewChild('links')
+  public linksContainer: ElementRef;
+
+  public toggleNavigationBar() {
+    this.linksContainer.nativeElement.classList.toggle('links--show');
+  }
+}
