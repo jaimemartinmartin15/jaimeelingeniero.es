c6805e36d13d4f19890d546175d64ae29fd03a30
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-22 16:02:58 +0100

    Added techs section

--- src/pages-presentation/my-trayectory/my-trayectory.module.ts
+++ src/pages-presentation/my-trayectory/my-trayectory.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MyTrayectoryRoutingModule } from './my-trayectory-routing.module';
 import { MyTrayectoryComponent } from './my-trayectory.component';
+import { TechnologiesSvgModule } from 'src/svg/generated/technologies-svg.module';
 
 @NgModule({
-  imports: [CommonModule, MyTrayectoryRoutingModule],
+  imports: [CommonModule, MyTrayectoryRoutingModule, TechnologiesSvgModule],
   declarations: [MyTrayectoryComponent],
 })
 export class MyTrayectoryModule {}
