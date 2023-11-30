92271bc18fa9249a033c315ba322eeac14bb976f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-30 19:37:48 +0100

    Added contact in my-trayectory page

--- src/pages-presentation/my-trayectory/my-trayectory.module.ts
+++ src/pages-presentation/my-trayectory/my-trayectory.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MyTrayectoryRoutingModule } from './my-trayectory-routing.module';
-import { MyTrayectoryComponent } from './my-trayectory.component';
+import { ContactIconsSvgModule } from 'src/svg/generated/contact-icons-svg.module';
 import { TechnologiesSvgModule } from 'src/svg/generated/technologies-svg.module';
 import { BulletsComponent } from './bullets/bullets.component';
+import { MyTrayectoryRoutingModule } from './my-trayectory-routing.module';
+import { MyTrayectoryComponent } from './my-trayectory.component';
 
 @NgModule({
-  imports: [CommonModule, MyTrayectoryRoutingModule, TechnologiesSvgModule],
+  imports: [CommonModule, MyTrayectoryRoutingModule, TechnologiesSvgModule, ContactIconsSvgModule],
   declarations: [MyTrayectoryComponent, BulletsComponent],
 })
 export class MyTrayectoryModule {}
