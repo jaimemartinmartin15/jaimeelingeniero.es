33a5a747c59d679d34ed87df799759a4704688df
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-12-01 17:44:18 +0100

    Removed code and set a message to redirect new app

--- src/pages-weather/rain/rain-routing.module.ts
+++ src/pages-weather/rain/rain-routing.module.ts
@@ -11,7 +11,6 @@ const routes: Routes = [
       metaTags: {
         description: 'En esta página podrás ver lo que ha llovido en la zona que vivo a lo largo del tiempo.',
         keywords: ['lluvia', 'tiempo', 'precipitacion'],
-        favIcon: 'assets/pages-weather/rain/favicons/favicon-{size}x{size}.png',
       },
     },
   },
